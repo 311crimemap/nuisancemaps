75f0355ba53a934aa251db6b8b200937f28f606b
vergeman <alan.verga@gmail.com>
2024-01-23 16:47:16 -0600

    clean up unused imports

--- api/src/main/java/com/quirkshop/nuisancemaps/repository/SourceRepository.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/repository/SourceRepository.java
@@ -1,7 +1,5 @@
 package com.quirkshop.nuisancemaps.repository;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
