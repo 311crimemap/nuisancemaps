06ad1f2965c7b9a2e345b9abc6247a4bb4623cd5
vergeman <alan.verga@gmail.com>
2024-01-23 22:04:27 -0600

    SourceLoaderService with loadJSON() and SourceLoaderServiceTest

---
+++ api/src/test/java/com/quirkshop/nuisancemaps/service/SourceLoaderServiceTest.java
@@ -0,0 +1,32 @@
+package com.quirkshop.nuisancemaps.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.quirkshop.nuisancemaps.NuisancemapsApplication;
+import com.quirkshop.nuisancemaps.model.Source;
+
+@SpringBootTest(classes = NuisancemapsApplication.class)
+public class SourceLoaderServiceTest {
+
+    @Autowired
+    private SourceLoaderService sourceLoaderService;
+
+    @Test
+    public void loadJSONTest() {
+        assertThat(sourceLoaderService.getSourceMap()).isNull();
+        sourceLoaderService.loadJSON("data/source_config.json");
+        assertThat(sourceLoaderService.getSourceMap()).isNotNull();
+        HashMap<Integer, Source> sourceMap = sourceLoaderService.getSourceMap();
+        Source s= sourceMap.get(1);
+        assertThat(s).isInstanceOf(Source.class);
+        Map<String, Object> m = s.getMapping();
+        assertThat(m.get("report-num").toString()).isEqualTo("incident_report_number");
+    }
+}
