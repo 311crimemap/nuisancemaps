a5ceb37634643a25950a43bebed2a03f49dc2da3
vergeman <alan.verga@gmail.com>
2024-02-01 09:20:13 -0600

    WorkerApplication / NuisancemapsApplication: add ComponentScan filters to isolate loaded classes *  e.g. disable scheduling on api Moved shared @Bean dependency to RestTemplateConfig class

--- api/src/main/java/com/quirkshop/nuisancemaps/NuisancemapsApplication.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/NuisancemapsApplication.java
@@ -5,22 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import com.quirkshop.nuisancemaps.repository.SourceRepository;
 import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
 
-
 @SpringBootApplication
+@ComponentScan(
+		// Excludes worker, @EnableScheduling annotation (api and related backend only)
+		excludeFilters = {
+				@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WorkerApplication.class)
+		})
 public class NuisancemapsApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(NuisancemapsApplication.class);
 
-	/*
-	 * @Autowired
-	 * private EntityManager entityManager;
-	 */
 	public static void main(String[] args) {
 
 		// output before "spring" logo
@@ -32,12 +32,6 @@ public class NuisancemapsApplication {
 		// output after load
 		System.out.println("println post");
 		log.info("log post");
-	}
-
-	// used in DataJobRequest
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
 	}
 
 	@Bean
--- api/src/main/java/com/quirkshop/nuisancemaps/WorkerApplication.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/WorkerApplication.java
@@ -5,9 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan(
+		// no api controllers, scheduled service and backend related
+		excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = NuisancemapsApplication.class),
+            @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = RestController.class)
+		})
 @ConditionalOnProperty(value = "app.scheduling.enabled", matchIfMissing = true, havingValue = "true")
 @EnableScheduling
 public class WorkerApplication {
