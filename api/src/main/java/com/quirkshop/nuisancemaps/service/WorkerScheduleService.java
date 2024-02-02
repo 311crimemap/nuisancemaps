265b62b606d9f8c00e0ed9e2f908441489007cbf
vergeman <alan.verga@gmail.com>
2024-02-02 17:20:34 -0600

    DataService: change createData to set numFetched and numProcessed internal DataJob; return void

--- api/src/main/java/com/quirkshop/nuisancemaps/service/WorkerScheduleService.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/service/WorkerScheduleService.java
@@ -152,7 +152,7 @@ public class WorkerScheduleService {
         dataJobRepository.save(datajob);
         log.info("createData()");
 
-        int num = dataservice.createData(source, datajob, json);
+        dataservice.createData(source, datajob, json);
 
         // if high error rate, mark job as error and stop future jobs
         if (datajob.getStatus() == DataJobStatus.ERROR) {
@@ -161,10 +161,9 @@ public class WorkerScheduleService {
         }
 
         datajob.setStatus(DataJobStatus.COMPLETED);
-        datajob.setNumResults(num);
         dataJobRepository.save(datajob);
 
-        if (num == 0) {
+        if (datajob.getNumFetched() == 0) {
             return;
         }
 
