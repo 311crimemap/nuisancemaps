ec59d576372181017ea1e4b948e358306304210f
vergeman <alan.verga@gmail.com>
2024-02-05 15:22:18 -0600

    Misc fix formatting

--- api/src/main/java/com/quirkshop/nuisancemaps/repository/SourceRepository.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/repository/SourceRepository.java
@@ -1,24 +1,30 @@
 package com.quirkshop.nuisancemaps.repository;
 
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.LockModeType;
 
 import com.quirkshop.nuisancemaps.model.Source;
 
 @Repository
 public interface SourceRepository extends CrudRepository<Source, Integer> {
 
-        // auto implemented
-        public Source findOneByUrl(String url);
+    // auto implemented
+    public Source findOneByUrl(String url);
 
-        @Transactional
-        default Source findOrCreate(Source source) {
-                Source s = findOneByUrl(source.getUrl());
-                if (s != null) {
-                        return s;
-                }
-                s = save(source);
-                return s;
+    @Transactional
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    default Source findOrCreate(Source source) {
+        Source s = findOneByUrl(source.getUrl());
+        if (s != null) {
+            return s;
         }
+        s = save(source);
+        return s;
+    }
+
 }
