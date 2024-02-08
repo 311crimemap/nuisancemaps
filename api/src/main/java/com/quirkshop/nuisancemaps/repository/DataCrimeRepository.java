fc6a00ea51ad8dcd354a508f767054f6c9696dc9
vergeman <alan.verga@gmail.com>
2024-02-08 12:45:18 -0600

    Data repositories: add findOneByReportNumIn query method

--- api/src/main/java/com/quirkshop/nuisancemaps/repository/DataCrimeRepository.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/repository/DataCrimeRepository.java
@@ -3,10 +3,14 @@ package com.quirkshop.nuisancemaps.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.quirkshop.nuisancemaps.model.DataCrime;
 
 @Repository
 public interface DataCrimeRepository extends CrudRepository<DataCrime, Integer> {
-        // auto implemented
-        DataCrime findOneByReportNum(String reportNum);
+    // auto implemented
+    DataCrime findOneByReportNum(String reportNum);
+
+    List<DataCrime> findAllByReportNumIn(List<String> reportNums);
 }
--- api/src/test/java/com/quirkshop/nuisancemaps/repository/DataCrimeRepositoryTest.java
+++ api/src/test/java/com/quirkshop/nuisancemaps/repository/DataCrimeRepositoryTest.java
@@ -3,6 +3,10 @@ package com.quirkshop.nuisancemaps.repository;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +26,7 @@ public class DataCrimeRepositoryTest {
 
     @Test
     @Transactional
-    public void DataRepositoryFindByReportNum() throws Exception {
+    public void DataRepositoryFindByReportNumTest() throws Exception {
         Source s = new Source("category", "description", "url");
         sourceRepository.save(s);
         DataCrime d = new DataCrime(s);
@@ -35,5 +39,23 @@ public class DataCrimeRepositoryTest {
         DataCrime x2 = datacrime_repo.findOneByReportNum("abc");
         assertThat(x.getReportNum()).isEqualTo("123");
         assertThat(x2.getReportNum()).isEqualTo("abc");
+    }
+
+    @Test
+    @Transactional
+    public void DataRepositoryFindAllByReportNumTest() throws Exception {
+        Source s = new Source("category", "description", "url");
+        sourceRepository.save(s);
+        DataCrime d = new DataCrime(s);
+        DataCrime d2 = new DataCrime(s);
+        d.setReportNum("123");
+        d2.setReportNum("abc");
+        datacrime_repo.save(d);
+        datacrime_repo.save(d2);
+
+        List<String> dataCrimes = new ArrayList<String>(List.of( "123", "abc" ));
+        List<DataCrime> results = datacrime_repo.findAllByReportNumIn(dataCrimes);
+        assertThat(results.get(0).getReportNum()).isEqualTo("123");
+        assertThat(results.get(1).getReportNum()).isEqualTo("abc");
     }
 }
