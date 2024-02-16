8022d3aa45690394bbc064ad6f987fb106834d98
vergeman <alan.verga@gmail.com>
2024-02-15 19:37:27 -0600

    update CORS hosts and ports with example App.tsx: add basic CORS fetch check

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,32 +4,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [count, setCount] = useState(0)
+
+    const testFetch = async () => {
+        const response = await fetch("http://localhost:8080/datajobs");
+        const dataJobs = await response.json();
+        console.log(dataJobs);
+
+        console.log(import.meta.env);
+        console.log(import.meta.env.MODE);
+        console.log(import.meta.env.BASE_URL);
+
+    };
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <button onClick={() => testFetch()}>
+                    Fetch
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
 }
 
 export default App
