41a34b0d75dced060999ffd3182c310af0d56053
vergeman <alan.verga@gmail.com>
2024-02-19 15:51:22 -0600

    MapComponent.jsx: refactor out style.json to baseMapStyle.json App.jsx: replace fetch datacrimes and data311 to Maplibre sources Add style layers for datacrimes, data311

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,12 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 
 import MapComponent from "./MapComponent";
 
 function App() {
 
-    const dataCrimesURL = "http://localhost:8080/datacrimes?limit=500";
-    const data311sURL = "http://localhost:8080/data311s?limit=500";
-
-    const [dataCrimes, setDataCrimes] = useState([]);
-    const [data311s, setData311s] = useState([]);
-
-    const get_data = (url: string) =>  {
-        return fetch(url).then( res => res.json());
-    };
-
-
-    useEffect(() => {
-
-        Promise.all([get_data(dataCrimesURL), get_data(data311sURL)])
-            .then(res => {
-                setDataCrimes(res[0]);
-                setData311s(res[1]);
-            });
-    }, []);
-
-
     return (
         <>
-            <MapComponent dataCrimes={dataCrimes} data311s={data311s} />
+            <MapComponent />
         </>
     )
 }
--- web/src/MapComponent.tsx
+++ web/src/MapComponent.tsx
@@ -1,28 +1,51 @@
 import { useState, useEffect, useRef } from 'react'
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
-import styleJSON from "./style.json";
+import baseMapStyleJSON from "./assets/baseMapStyle.json";
+import dataCrimesStyleJSON from "./assets/datacrimes_style.json";
+import data311sStyleJSON from "./assets/data311s_style.json";
 
 export default function MapComponent(props: any) {
 
     //const mapRef = useRef<maplibregl.Map>();
     const [map, setMap] = useState();
 
-    useEffect( () => {
+    useEffect(() => {
 
         const style = {
             glyphs: "https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf",
             version: 8,
             sources: {
-                protomaps : {
+                protomaps: {
                     type: "vector",
                     url: "https://api.protomaps.com/tiles/v3.json?key=a8e24b8d978e4df3",
                     attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a> | <a href="https://protomaps.com/">Protomaps',
                     minzoom: 2,
                     maxzoom: 12,
+                },
+                datacrimes: {
+                    type: "geojson",
+                    data: "http://localhost:8080/datacrimes.geojson?limit=500",
+                    cluster: true,
+                    clusterMaxZoom: 14, // Max zoom to cluster points on
+                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+                },
+
+                data311s: {
+                    type: "geojson",
+                    data: "http://localhost:8080/data311s.geojson?limit=500",
+                    cluster: true,
+                    clusterMaxZoom: 14, // Max zoom to cluster points on
+                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
                 }
+
             },
-            ...styleJSON
+            layers: [
+                ...baseMapStyleJSON,
+                ...dataCrimesStyleJSON,
+                ...data311sStyleJSON
+            ]
+
         };
 
         const _map = new maplibregl.Map({
@@ -35,9 +58,62 @@ export default function MapComponent(props: any) {
         //_map.showTileBoundaries = true;
         setMap(_map);
 
+
+        _map.on('load', () => {
+
+            for (const dataset of ['datacrime', 'data311']) {
+                // inspect a cluster on click
+                _map.on('click', `clusters-${dataset}`, async (e) => {
+                    const features = _map.queryRenderedFeatures(e.point, {
+                        layers: [`clusters-${dataset}`]
+                    });
+
+                    const clusterId = features[0].properties.cluster_id;
+                    const source = features[0].source;
+
+                    const zoom = await _map.getSource(source).getClusterExpansionZoom(clusterId);
+                    _map.easeTo({
+                        center: features[0].geometry.coordinates,
+                        zoom
+                    });
+                });
+
+                // When a click event occurs on a feature in
+                // the unclustered-point layer, open a popup at
+                // the location of the feature, with
+                // description HTML from its properties.
+                _map.on('click', `unclustered-point-${dataset}`, (e) => {
+                    const coordinates = e.features[0].geometry.coordinates.slice();
+                    const category = e.features[0].properties.category;
+
+                    /*
+                       reportNum
+                       category
+                       location
+                       reportedAt
+                    */
+
+                    // Ensure that if the map is zoomed out such that
+                    // multiple copies of the feature are visible, the
+                    // popup appears over the copy being pointed to.
+                    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                    }
+
+                    new maplibregl.Popup()
+                        .setLngLat(coordinates)
+                        .setHTML(
+                            `${category}`
+                        )
+                        .addTo(_map);
+                });
+            }
+
+        });
+
         //mapRef.current = _map;
 
-        return(() => {
+        return (() => {
 
             if (map) {
                 map.remove();
@@ -48,40 +124,7 @@ export default function MapComponent(props: any) {
     }, [])
 
 
-    /*
-     *  MARKER
-     */
-    if(map) {
-
-        props.dataCrimes.forEach( data => {
-            // create the popup
-            const popup = new maplibregl.Popup({ offset: 25 }).setHTML(
-                `<h3>${data.category}</h3>`
-            );
-
-            const el = document.createElement('div');
-            el.className = 'marker';
-            el.style.backgroundImage =
-                `url(https://placekitten.com/g/50/50/)`;
-            el.style.width = `50px`;
-            el.style.height = `50px`;
-
-            new maplibregl.Marker({ element: el })
-                .setLngLat({lng: data.longitude, lat: data.latitude})
-                .setPopup(popup)
-                .addTo(map);
-
-
-        });
-
-
-        console.log("Crimes", props.dataCrimes);
-        console.log("311", props.data311s);
-    }
-
-
-
     return (
-        <div id='map' style={{width: '100vw', height: '100vh'}}></div>
+        <div id='map' style={{ width: '100vw', height: '100vh' }}></div>
     )
 }
