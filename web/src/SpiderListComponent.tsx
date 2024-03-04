451e35a7e96d6666c8074307a479f9a77a5a972d
vergeman <alan.verga@gmail.com>
2024-03-04 17:19:16 -0600

    Add initial layout (merge topic/layout/init)
    * add SpiderListComponent stub
    
    * rename BaseComponent to BottomSheetComponent
    
    * move styling into css; make map full size
    * rearrange Component order
    * add Navbar stub
    
    * cleanup unused css; add basic component ids
    
    * install react-router package
    * add basic Router scaffold with App root
    * add error-page.tsx

---
+++ web/src/SpiderListComponent.tsx
@@ -0,0 +1,8 @@
+/*
+ * TODO:
+ * toggle visible and pass spider cluster elements when spider cluster is clicked
+ * make sure to hook into spider close event
+ */
+export default function SpiderListComponent() {
+    return <div id="spider-list-component">Spider List Component</div>
+}
