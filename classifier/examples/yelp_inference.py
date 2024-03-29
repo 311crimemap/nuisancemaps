527ea60a35ef934ddc4561941c177871400f70db
vergeman <alan.verga@gmail.com>
2024-03-29 15:36:40 -0500

    remove trivial /examples

--- classifier/examples/yelp_inference.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-model = AutoModelForSequenceClassification.from_pretrained("fine_tuned_model", from_tf=False)
-
-tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
-
-from transformers import pipeline
-clf = pipeline("text-classification", model, tokenizer=tokenizer)
-
-#answer = clf("text", return_all_scores=True)
