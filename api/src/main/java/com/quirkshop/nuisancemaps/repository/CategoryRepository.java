e29779812bc634e3f6d6c57fe011dc7f3478013a
vergeman <alan.verga@gmail.com>
2024-03-31 15:59:26 -0500

    add CategoryRepository.java

---
+++ api/src/main/java/com/quirkshop/nuisancemaps/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.quirkshop.nuisancemaps.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import com.quirkshop.nuisancemaps.model.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+    // auto implemented
+
+    List<Category> findAllByDataType(String dataType);
+}
