5a95afcf2a71e678a238dc0d20d687b0d8c4262c
vergeman <alan.verga@gmail.com>
2024-05-07 15:35:03 -0500

    [api] add orgCategory to DTO output

--- api/src/main/java/com/quirkshop/nuisancemaps/dto/PropertiesDTO.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/dto/PropertiesDTO.java
@@ -2,17 +2,21 @@ package com.quirkshop.nuisancemaps.dto;
 
 import java.time.LocalDateTime;
 
+import com.quirkshop.nuisancemaps.model.Category;
+
 public class PropertiesDTO {
     private String reportNum;
     private String reportCategory;
     private String location;
+    private Category orgCategory;
     private LocalDateTime reportedAt;
 
-    public PropertiesDTO(String reportCategory, String location, LocalDateTime reportedAt, String reportNum) {
+    public PropertiesDTO(String reportCategory, String location, LocalDateTime reportedAt, String reportNum, Category orgCategory) {
         this.reportCategory = reportCategory;
         this.location = location;
         this.reportedAt = reportedAt;
         this.reportNum = reportNum;
+        this.orgCategory = orgCategory;
     }
 
     public String getReportNum() {
@@ -45,5 +49,13 @@ public class PropertiesDTO {
 
     public void setReportedAt(LocalDateTime reportedAt) {
         this.reportedAt = reportedAt;
+    }
+
+    public Category getOrgCategory() {
+        return orgCategory;
+    }
+
+    public void setOrgCategory(Category orgCategory) {
+        this.orgCategory = orgCategory;
     }
 }
