f9445f735a66bc407a1aad2ecbeb51169e565798
vergeman <alan.verga@gmail.com>
2024-06-05 10:02:38 -0500

    add DropDownFilter to ControlBar

--- web/src/components/ControlBar/ControlBar.tsx
+++ web/src/components/ControlBar/ControlBar.tsx
@@ -1,18 +1,34 @@
-import { useState, useEffect, useMemo, useRef } from 'react'
-import {Search} from "./Search"
-import {DateComponent } from "./DateComponent";
+import { useState, useEffect, useMemo, useRef } from "react";
+import { Search } from "./Search";
+import { DateComponent } from "./DateComponent";
 import { DropDownFilter } from "./DropDownFilter";
 
-export default function ControlBar({filterDate, filterDateDispatcher}) {
+export default function ControlBar({
+  activeCategories,
+  activeCategoriesDispatcher,
+  filterDate,
+  filterDateDispatcher,
+}) {
+  if (activeCategories.length == 0) return null;
 
-    return(
-        <div className = "navbar bg-base-100">
-            <Search />
-            <DateComponent filterDate={filterDate} filterDateDispatcher={filterDateDispatcher}/>
-            <DropDownFilter type="crime" />
-            <DropDownFilter type="311" />
-            {
-                /*
+  return (
+    <div className="navbar bg-base-100">
+      <Search />
+      <DateComponent
+        filterDate={filterDate}
+        filterDateDispatcher={filterDateDispatcher}
+      />
+      <DropDownFilter
+        type="crime"
+        activeCategories={activeCategories}
+        activeCategoriesDispatcher={activeCategoriesDispatcher}
+      />
+      <DropDownFilter
+        type="311"
+        activeCategories={activeCategories}
+        activeCategoriesDispatcher={activeCategoriesDispatcher}
+      />
+      {/*
                         <Sidebar
                             map={map}
                             activeReportNum={activeReportNum}
@@ -22,9 +38,7 @@ export default function ControlBar({filterDate, filterDateDispatcher}) {
                             filterDate={filterDate}
                             filterDateDispatcher={filterDateDispatcher}
                         />
-                */
-            }
-
-        </div>
-    )
+                */}
+    </div>
+  );
 }
--- web/src/components/ControlBar/DropDownFilter.tsx
+++ web/src/components/ControlBar/DropDownFilter.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import CheckBoxGroup from "./CheckBoxFilters/CheckBoxGroup.js";
 
 /*
  * use dropdown button versus <details> / <summary> tags
  * because dropdown will close on click elsewhere;
  * detail / summary remains open unless explicitly clicked to minimize parent
  */
-export function DropDownFilter(props: any) {
+export function DropDownFilter({type, activeCategories, activeCategoriesDispatcher}) {
     const [icon, setIcon] = useState(faChevronUp);
+    const parent = activeCategories.find((cat) => cat.id == type);
 
     return (
 
@@ -17,14 +19,19 @@ export function DropDownFilter(props: any) {
             <div tabIndex={0} role="button" className="btn btn-sm"
                 onBlur={() => setIcon(faChevronUp)}
                 onFocus={() => setIcon(faChevronDown)}>
-                {props.type}
+                {type}
                 <FontAwesomeIcon icon={icon} />
             </div>
 
             <ul className="dropdown-content z-[1] menu shadow p-2 bg-base-100 rounded-box w-52"
                 onFocus={() => setIcon(faChevronDown)}
             >
-                Stuff
+                <CheckBoxGroup
+                    key={`group-${parent.id}`}
+                    parent={parent}
+                    categories={activeCategories}
+                    activeCategoriesDispatcher={activeCategoriesDispatcher}
+                />
 
             </ul>
 
