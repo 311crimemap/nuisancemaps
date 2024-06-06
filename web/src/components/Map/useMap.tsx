d4a48be39947b68e3d2d55478285a251ae9d30b7
vergeman <alan.verga@gmail.com>
2024-06-06 14:38:37 -0500

    revise naming approach for consistency between data source and layer id

--- web/src/components/Map/useMap.tsx
+++ web/src/components/Map/useMap.tsx
@@ -6,236 +6,230 @@ import dataCrimesStyleJSON from "../../assets/datacrimes_style.json";
 import data311sStyleJSON from "../../assets/data311s_style.json";
 
 export default function useMap(props) {
+  //const mapRef = useRef<maplibregl.Map>();
+  const [map, setMap] = useState(null);
+  enum DATASOURCES {
+    Data311s = "data311s",
+    DataCrimes = "dataCrimes",
+  }
 
-    //const mapRef = useRef<maplibregl.Map>();
-    const [map, setMap] = useState(null);
+  useEffect(() => {
+    console.log("[useMap] Hook Init");
 
-    useEffect(() => {
-        console.log("[useMap] Hook Init")
+    if (!props.isDataLoaded) return; //NB: wait until data fetched before creating map
 
-        if (!props.isDataLoaded) return; //NB: wait until data fetched before creating map
+    const dataSources = {
+      [DATASOURCES.Data311s]: {
+        type: "geojson",
+        data: props.data311s,
+        cluster: true,
+        clusterMaxZoom: 18, // Max zoom to cluster points on
+        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+      },
+      [DATASOURCES.DataCrimes]: {
+        type: "geojson",
+        data: props.dataCrimes,
+        cluster: true,
+        clusterMaxZoom: 18, // Max zoom to cluster points on
+        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+      },
+    };
 
-        const style = {
-            glyphs: "https://alanverga.com/basemaps-assets/fonts/{fontstack}/{range}.pbf",
-            version: 8,
-            sources: {
-                protomaps: {
-                    type: "vector",
-                    url: "https://api.protomaps.com/tiles/v3.json?key=a8e24b8d978e4df3",
-                    attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a> | <a href="https://protomaps.com/">Protomaps',
-                    minzoom: 2,
-                    maxzoom: 12,
-                },
+    const style = {
+      glyphs:
+        "https://alanverga.com/basemaps-assets/fonts/{fontstack}/{range}.pbf",
+      version: 8,
+      sources: {
+        protomaps: {
+          type: "vector",
+          url: "https://api.protomaps.com/tiles/v3.json?key=a8e24b8d978e4df3",
+          attribution:
+            '© <a href="https://openstreetmap.org">OpenStreetMap</a> | <a href="https://protomaps.com/">Protomaps',
+          minzoom: 2,
+          maxzoom: 12,
+        },
+        ...dataSources,
+      },
+      layers: [
+        ...baseMapStyleJSON,
+        ...dataCrimesStyleJSON,
+        ...data311sStyleJSON,
+      ],
+    };
 
-                datacrimes: {
-                    type: "geojson",
-                    data: props.dataCrimes,
-                    cluster: true,
-                    clusterMaxZoom: 18, // Max zoom to cluster points on
-                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-                },
+    console.log("NEW MAP");
+    const _map = new maplibregl.Map({
+      container: "map",
+      //center: [-97.7171, 30.2944], // starting position [lng, lat]
+      center: props.position.center,
+      zoom: 12, // starting zoom
+      style,
+    });
 
-                data311s: {
-                    type: "geojson",
-                    data: props.data311s,
-                    cluster: true,
-                    clusterMaxZoom: 18, // Max zoom to cluster points on
-                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-                }
+    //_map.showTileBoundaries = true;
 
-            },
-            layers: [
-                ...baseMapStyleJSON,
-                ...dataCrimesStyleJSON,
-                ...data311sStyleJSON
-            ]
+    _map.on("load", async () => {
+      console.log("Load");
+    });
 
-        };
+    for (const dataset of Object.values(DATASOURCES)) {
+      // When a click event occurs on a feature in
+      // the unclustered-point layer, open a popup at
+      // the location of the feature, with
+      // description HTML from its properties.
 
-        console.log("NEW MAP");
-        const _map = new maplibregl.Map({
-            container: 'map',
-            //center: [-97.7171, 30.2944], // starting position [lng, lat]
-            center: props.position.center,
-            zoom: 12, // starting zoom
-            style
-        });
+      _map.on("click", `point-${dataset}`, (e) => {
+        console.log("CLICK unclustered");
+        const source = e.features[0].source;
+        const layer = e.features[0].layer;
+        const circleLayerID = layer.id.includes("311")
+          ? `point-circle-${dataset}`
+          : `point-circle-${dataset}`;
 
-        //_map.showTileBoundaries = true;
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const reportCategory = e.features[0].properties.reportCategory;
+        const reportNum = e.features[0].properties.reportNum;
+        const features = e.features;
 
-        _map.on('load', async () => {
-            console.log("Load");
-        });
-
-
-        for (const dataset of ['datacrime', 'data311']) {
-
-            // When a click event occurs on a feature in
-            // the unclustered-point layer, open a popup at
-            // the location of the feature, with
-            // description HTML from its properties.
-
-            _map.on('click', `unclustered-point-${dataset}`, (e) => {
-                console.log("CLICK unclustered");
-                const source = e.features[0].source;
-                const layer = e.features[0].layer;
-                const circleLayerID = layer.id.includes("311") ?
-                                      "circle-data311-layer" : "circle-datacrime-layer";
-
-                const coordinates = e.features[0].geometry.coordinates.slice();
-                const reportCategory = e.features[0].properties.reportCategory;
-                const reportNum = e.features[0].properties.reportNum;
-                const features = e.features;
-
-                // Ensure that if the map is zoomed out such that
-                // multiple copies of the feature are visible, the
-                // popup appears over the copy being pointed to.
-                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                }
-
-                //increae icon size
-                _map.setLayoutProperty(layer.id, 'text-size',
-                    [
-                        'match',
-                        ['get', 'reportNum'], reportNum, // get the feature id
-                        30, //new text-size
-                        18  //default - needs to be constant not layer reference (since it will change here)
-                    ]
-                )
-
-                //increase background circle radius
-                _map.setPaintProperty(circleLayerID, 'circle-radius',
-                    [
-                        'match',
-                        ['get', 'reportNum'], reportNum,
-                        24, //new radius
-                        16  //default
-                    ]
-                )
-
-                new maplibregl.Popup()
-                    .setLngLat(coordinates)
-                    .setHTML(
-                        `${reportCategory}`
-                    )
-                    .addTo(_map);
-
-                props.setActiveReportNum(reportNum);
-
-                const featureList = {
-                    source,
-                    features
-                }
-
-                props.setActiveFeatureList(featureList);
-            });
-
-            //click on a clustered point
-            _map.on('click', `clusters-${dataset}`, async (e) => {
-                console.log("CLICK Cluster", e);
-
-                const source = e.features[0].source;
-                const cluster_id = e.features[0].properties.cluster_id;
-                const coordinates = e.features[0].geometry.coordinates;
-                const point_count = e.features[0].properties.point_count;
-
-                const clusterSource = _map.getSource(source);
-
-                //TODO: consistent layer id /source name
-                const zoom = await _map.getSource(`${dataset}s`).getClusterExpansionZoom(cluster_id);
-                _map.easeTo({
-                    center: coordinates,
-                    zoom
-                });
-
-
-                //1. get list of individual elements in cluster (ids)
-                //2. set open
-
-                const features = await clusterSource.getClusterLeaves(cluster_id, point_count, 0);
-                console.log("FE", features);
-                const featureList = {
-                    source,
-                    features
-                }
-
-                props.setActiveFeatureList(featureList);
-            });
-
+        // Ensure that if the map is zoomed out such that
+        // multiple copies of the feature are visible, the
+        // popup appears over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
 
+        //increae icon size
+        _map.setLayoutProperty(layer.id, "text-size", [
+          "match",
+          ["get", "reportNum"],
+          reportNum, // get the feature id
+          30, //new text-size
+          18, //default - needs to be constant not layer reference (since it will change here)
+        ]);
 
-        /*
-         * "general" click handler
-         * this is clicking anywhere not a "point" (cluster / uncluster)
-         * so anywhere that's not base source protomaps
-         * used to clear displays like feature list
-         */
-        _map.on('click', (e) => {
+        //increase background circle radius
+        _map.setPaintProperty(circleLayerID, "circle-radius", [
+          "match",
+          ["get", "reportNum"],
+          reportNum,
+          24, //new radius
+          16, //default
+        ]);
 
-            const features = _map.queryRenderedFeatures(e.point)
-                                 .filter(f => f.source != "protomaps");
+        new maplibregl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(`${reportCategory}`)
+          .addTo(_map);
 
-            console.log("GEN CLICK", features);
+        props.setActiveReportNum(reportNum);
 
-            //TODO: refactor this once default values figured out
+        const featureList = {
+          source,
+          features,
+        };
 
-            if (features.length) {
-                if (features[0].source.includes("311")) {
-                    _map.setLayoutProperty('unclustered-point-datacrime', 'text-size', 18)
-                    _map.setPaintProperty('circle-datacrime-layer', 'circle-radius', 16);
-                } else {
-                    _map.setLayoutProperty('unclustered-point-data311', 'text-size', 18)
-                    _map.setPaintProperty('circle-data311-layer', 'circle-radius', 16);
-                }
-            }
+        props.setActiveFeatureList(featureList);
+      });
 
-            //clear all
-            if (features.length === 0) {
+      //click on a clustered point
+      _map.on("click", `clusters-${dataset}`, async (e) => {
+        console.log("CLICK Cluster", e);
 
-                //clear, turn off anything in previous click handlers
-                _map.setLayoutProperty('unclustered-point-datacrime', 'text-size', 18)
-                _map.setLayoutProperty('unclustered-point-data311', 'text-size', 18)
+        const source = e.features[0].source;
+        const cluster_id = e.features[0].properties.cluster_id;
+        const coordinates = e.features[0].geometry.coordinates;
+        const point_count = e.features[0].properties.point_count;
 
-                _map.setPaintProperty('circle-datacrime-layer', 'circle-radius', 16);
-                _map.setPaintProperty('circle-data311-layer', 'circle-radius', 16);
+        const clusterSource = _map.getSource(source);
 
-
-                props.setActiveFeatureList({});
-
-            }
-
-        })
-
-
-        _map.on('moveend', async () => {
-
-            const position = {
-                zoom: _map.getZoom(),
-                center: _map.getCenter(),
-                bounds: _map.getBounds()
-            };
-
-            props.setPosition(position);
-        })
-
-        _map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
-
-        //mapRef.current = _map;
-        setMap(_map);
-
-        return (() => {
-
-            if (_map) {
-                console.log("[useMap] Remove")
-                _map.remove();
-                //maplibregl.removeProtocol("pmtiles");
-            }
+        //TODO: consistent layer id /source name
+        const zoom = await _map
+          .getSource(dataset)
+          .getClusterExpansionZoom(cluster_id);
+        _map.easeTo({
+          center: coordinates,
+          zoom,
         });
 
+        //1. get list of individual elements in cluster (ids)
+        //2. set open
 
-    }, [props.isDataLoaded])
+        const features = await clusterSource.getClusterLeaves(
+          cluster_id,
+          point_count,
+          0
+        );
 
-    return map;
+        const featureList = {
+          source,
+          features,
+        };
+
+        props.setActiveFeatureList(featureList);
+      });
+    }
+
+    /*
+     * "general" click handler
+     * this is clicking anywhere not a "point" (cluster / uncluster)
+     * so anywhere that's not base source protomaps
+     * used to clear displays like feature list
+     */
+    _map.on("click", (e) => {
+      const features = _map
+        .queryRenderedFeatures(e.point)
+        .filter((f) => f.source != "protomaps");
+
+      console.log("GEN CLICK", features);
+
+      //TODO: refactor this once default values figured out
+
+      if (features.length) {
+        if (features[0].source.includes("311")) {
+          _map.setLayoutProperty("point-dataCrimes", "text-size", 18);
+          _map.setPaintProperty("point-circle-dataCrimes", "circle-radius", 16);
+        } else {
+          _map.setLayoutProperty("point-data311s", "text-size", 18);
+          _map.setPaintProperty("point-circle-data311s", "circle-radius", 16);
+        }
+      }
+
+      //clear all
+      if (features.length === 0) {
+        //clear, turn off anything in previous click handlers
+        _map.setLayoutProperty("point-dataCrimes", "text-size", 18);
+        _map.setLayoutProperty("point-data311s", "text-size", 18);
+
+        _map.setPaintProperty("point-circle-dataCrimes", "circle-radius", 16);
+        _map.setPaintProperty("point-circle-data311s", "circle-radius", 16);
+
+        props.setActiveFeatureList({});
+      }
+    });
+
+    _map.on("moveend", async () => {
+      const position = {
+        zoom: _map.getZoom(),
+        center: _map.getCenter(),
+        bounds: _map.getBounds(),
+      };
+
+      props.setPosition(position);
+    });
+
+    _map.addControl(new maplibregl.NavigationControl(), "bottom-right");
+
+    //mapRef.current = _map;
+    setMap(_map);
+
+    return () => {
+      if (_map) {
+        console.log("[useMap] Remove");
+        _map.remove();
+        //maplibregl.removeProtocol("pmtiles");
+      }
+    };
+  }, [props.isDataLoaded]);
+
+  return map;
 }
