b92b06b1a5220d10fbf40156b5adbe209c24a128
vergeman <alan.verga@gmail.com>
2024-06-07 12:33:26 -0500

    Geocoder component: disable showFullGeometry

--- web/src/components/ControlBar/Search.tsx
+++ web/src/components/ControlBar/Search.tsx
@@ -14,6 +14,7 @@ export function Search({ mapController }) {
           apiKey={API_KEY}
           mapController={mapController}
           country="us"
+          showFullGeometry={false}
           flyTo={{ speed: 30, duration: 2 }}
         />
       </div>
