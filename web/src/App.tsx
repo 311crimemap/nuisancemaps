c6a576aea0f3d3fccfe24489601ad5b8c79bb197
Alan Verga <alan.verga@gmail.com>
2024-06-07 19:29:48 -0500

    Topic/bugs/web 1 (#42)
    * preserve filters when reloading data
    
    * fix dropdown premature close on text click
    
    * App.tsx: add temporary default bound coordinates

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -17,8 +17,8 @@ function App() {
       lng: -97.7171,
     },
     bounds: {
-      _sw: { lat: null, lng: null },
-      _ne: { lat: null, lng: null },
+      _sw: { lat: 30.259261190163073, lng: -97.77095608156225 },
+      _ne: { lat: 30.30730791957427, lng: -97.68701127709589 },
     },
   });
 
@@ -116,16 +116,21 @@ function App() {
     Promise.all([
       getData(dataCrimesURL),
       getData(data311sURL),
-      getData(categoriesURL),
-    ]).then(([dataCrimes, data311s, categories]) => {
+      categories.length == 0
+        ? getData(categoriesURL)
+        : Promise.resolve(categories),
+    ]).then(([dataCrimes, data311s, dataCategories]) => {
       setDataCrimes(dataCrimes);
       setData311s(data311s);
-      setCategories(categories.data);
 
-      activeCategoriesDispatcher({
-        type: "init",
-        categories: Categories.buildHierarchy(categories.data),
-      });
+      //preserve any checked filters
+      if (categories.length == 0) {
+        setCategories(dataCategories.data);
+        activeCategoriesDispatcher({
+          type: "init",
+          categories: Categories.buildHierarchy(dataCategories.data),
+        });
+      }
 
       setIsDataLoaded(true);
     });
