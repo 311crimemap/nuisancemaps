f1108deb8cef5eff65a76978283e42cd47deed4b
vergeman <alan.verga@gmail.com>
2024-06-18 15:42:07 -0500

    CheckBoxLabel: add inclusiveCheck property if checkbox behavior includes text or not

--- web/src/components/ControlBar/CategoryDropDown/CheckBoxFilters/CheckBoxLabel.tsx
+++ web/src/components/ControlBar/CategoryDropDown/CheckBoxFilters/CheckBoxLabel.tsx
@@ -2,27 +2,42 @@ export default function CheckBoxLabel({
   category,
   categories,
   activeCategoriesDispatcher,
+  inclusiveCheck = true, //whether checkbox ste to trigger inclusive of clicking on text
 }) {
   const checkHandler = (e) => {
     activeCategoriesDispatcher({
       type: "toggleCheckBoxById",
       category,
     });
-
-    console.log("CLICK", category, category.id, category.checked);
   };
 
   return (
     <div>
-      <label>
-        <input
-          id={`${category.id}-checkbox`}
-          type="checkbox"
-          checked={category.checked}
-          onChange={checkHandler}
-        />
-        <span className="label-text pl-2">{`${category.text}`}</span>
-      </label>
+      {inclusiveCheck ? (
+        <label>
+          <input
+            id={`${category.id}-checkbox`}
+            type="checkbox"
+            checked={category.checked}
+            className="cursor-pointer"
+            onChange={checkHandler}
+          />
+          <span className="label-text pl-2">{`${category.text}`}</span>
+        </label>
+      ) : (
+        <>
+          <label>
+            <input
+              id={`${category.id}-checkbox`}
+              type="checkbox"
+              checked={category.checked}
+              className="cursor-pointer"
+              onChange={checkHandler}
+            />
+          </label>
+          <span className="label-text pl-2">{`${category.text}`}</span>
+        </>
+      )}
     </div>
   );
 }
