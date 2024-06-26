2bf4ff51e4663ff6cc77397c647759cf1f2c1504
vergeman <alan.verga@gmail.com>
2024-06-26 18:42:29 -0500

    DateComponent: add loading screen divider when filterDate.isBusy

--- web/src/components/ControlBar/DateDropDown/DateComponent.tsx
+++ web/src/components/ControlBar/DateDropDown/DateComponent.tsx
@@ -59,71 +59,82 @@ export function DateComponent({ filterDate, filterDateDispatcher }) {
     });
   }, [filterDate.date.startDate, filterDate.date.endDate]);
 
+  console.log("[DateComponent]", filterDate);
   return (
-        <DropDown>
-            <div>Date</div>
+    <DropDown>
+      <div>Date</div>
 
-            <div className="flex flex-col gap-6">
-                {/* Preset */}
-                <div className="flex flex-col gap-2">
-                    <label>
-                        {" "}
-                        <strong>Date Range Preset</strong>{" "}
-                    </label>
-                    <select
-                        id="presetDate"
-                        name="presetDate"
-                        className="select select-bordered w-full max-w-xs"
-                        onChange={(e) =>
-                            filterDateDispatcher({
-                                type: "calcDate",
-                                value: e.target.value,
-                            })
-                        }
-                    >
-                        <option disabled selected>
-                            Select timeframe from today
-                        </option>
-                        <option value="1"> 1 day</option>
-                        <option value="3"> 3 days</option>
-                        <option value="7"> 1 week</option>
-                        <option value="14"> 2 weeks</option>
-                        <option value="1 month"> 1 month </option>
-                        <option value="2 month"> 2 months </option>
-                        <option value="3 month"> 3 months </option>
-                        <option value="6 month"> 6 months </option>
-                    </select>
-                </div>
-                <div className="divider">OR</div>
+      <div className="flex flex-col gap-6">
+        {/* Preset */}
+        <div className="flex flex-col gap-2">
+          <label>
+            {" "}
+            <strong>Date Range Preset</strong>{" "}
+          </label>
+          <select
+            id="presetDate"
+            name="presetDate"
+            className="select select-bordered w-full"
+            onChange={(e) =>
+              debounceFilterDateDispatcher({
+                type: "calcDate",
+                value: e.target.value,
+              })
+            }
+          >
+            <option disabled selected>
+              Select timeframe from today
+            </option>
+            <option value="1"> 1 day</option>
+            <option value="3"> 3 days</option>
+            <option value="7"> 1 week</option>
+            <option value="14"> 2 weeks</option>
+            <option value="1 month"> 1 month </option>
+            <option value="2 month"> 2 months </option>
+            <option value="3 month"> 3 months </option>
+            <option value="6 month"> 6 months </option>
+          </select>
+        </div>
 
-                {/* Calendars */}
-                <div className="flex flex-col gap-4">
-                    <strong>Custom Date Range</strong>
-                    <div className="flex flex-col gap-2">
-                        <label for="start">Start</label>
-                        <InputDate
-                            id="startDate"
-                            name="startDate"
-                            minDate={startMinDate}
-                            maxDate={startMaxDate}
-                            inputDate={inputDate}
-                            setInputDate={setInputDate}
-                        />
-                    </div>
+        {/* Loading Screen Divider */}
+        <div className="divider">
+          {filterDate.isBusy ? (
+            <>
+              <span className="loading loading-spinner loading-lg"></span>
+            </>
+          ) : (
+            <>OR</>
+          )}
+        </div>
 
-                    <div className="flex flex-col gap-2">
-                        <label for="end">End</label>
-                        <InputDate
-                            id="endDate"
-                            name="endDate"
-                            minDate={endMinDate}
-                            maxDate={endMaxDate}
-                            inputDate={inputDate}
-                            setInputDate={setInputDate}
-                        />
-                    </div>
-                </div>
-            </div>
-        </DropDown>
-    );
+        {/* Calendars */}
+        <div className="flex flex-col gap-4">
+          <strong>Custom Date Range</strong>
+          <div className="flex flex-col gap-2">
+            <label for="start">Start</label>
+            <InputDate
+              id="startDate"
+              name="startDate"
+              minDate={startMinDate}
+              maxDate={startMaxDate}
+              inputDate={inputDate}
+              setInputDate={setInputDate}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label for="end">End</label>
+            <InputDate
+              id="endDate"
+              name="endDate"
+              minDate={endMinDate}
+              maxDate={endMaxDate}
+              inputDate={inputDate}
+              setInputDate={setInputDate}
+            />
+          </div>
+        </div>
+      </div>
+    </DropDown>
+  );
 }
