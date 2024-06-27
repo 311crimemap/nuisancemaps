5aafca4502e68a060dd56d218cb8ed31ff21a169
vergeman <alan.verga@gmail.com>
2024-06-27 14:56:56 -0500

    pages: refactor main.tsx to add paths and About, Cities component pages and paths

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,30 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Navbar from "./Navbar.tsx";
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import Cities from "./Cities.tsx";
+import About from "./About.tsx";
+import "./index.css";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        errorElement: <ErrorPage />
-    },
-    {
-        path: "/test",
-        element: <div>Hello world!</div>,
-    }
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <App />
+      </>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/cities",
+    element: (
+      <>
+        <Navbar />
+        <Cities />
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <Navbar />
+        <About />
+      </>
+    ),
+  },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Navbar />
-        <RouterProvider router={router} />
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
