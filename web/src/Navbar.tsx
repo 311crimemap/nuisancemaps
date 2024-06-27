c10671be4743e233592c6b88943b138c536f489a
vergeman <alan.verga@gmail.com>
2024-06-26 21:56:25 -0500

    Navbar initial styling

--- web/src/Navbar.tsx
+++ web/src/Navbar.tsx
@@ -1,9 +1,19 @@
-
 export default function Navbar() {
-
-    return (
-        <nav id="navbar">
-            Navbar
-        </nav>
-    )
+  return (
+    <nav data-theme="dracula" className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Nuisance Map</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Cities</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- web/src/components/ControlBar/ControlBar.tsx
+++ web/src/components/ControlBar/ControlBar.tsx
@@ -16,7 +16,7 @@ export default function ControlBar({
   if (activeCategories.length == 0) return null;
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 px-6 border-b">
       <div className="flex flex-col sm:flex-row w-full">
         <div className="w-full sm:w-auto mb-2 sm:mb-0">
           <Search mapController={mapController} />
