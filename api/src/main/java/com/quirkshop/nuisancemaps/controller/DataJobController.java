656ab7a4b7525ecf3e74a4cdea58334ef9bd424b
vergeman <alan.verga@gmail.com>
2024-07-14 18:44:38 -0500

    fix WORKER_QUERY_LIMIT env variable parse

--- api/src/main/java/com/quirkshop/nuisancemaps/controller/DataJobController.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/controller/DataJobController.java
@@ -38,7 +38,7 @@ public class DataJobController {
     @Autowired
     SourceRepository sourceRepository;
 
-    private final int PARAM_LIMIT = Integer.valueOf(System.getenv("WORKER_QUERY_LIMIT"));
+    private static final int PARAM_LIMIT = Integer.parseInt(System.getenv("WORKER_QUERY_LIMIT"));
 
     @CrossOrigin(origins = "${CORS_ORIGINS}")
     @GetMapping("/datajobs")
