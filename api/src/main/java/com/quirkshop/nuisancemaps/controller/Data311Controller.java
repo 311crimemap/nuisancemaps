5e0028c703b4435aeef6a289fdeae58678000513
vergeman <alan.verga@gmail.com>
2024-07-14 18:28:07 -0500

    refactor data* controllers for clarity, default parameters

--- api/src/main/java/com/quirkshop/nuisancemaps/controller/Data311Controller.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/controller/Data311Controller.java
@@ -3,6 +3,7 @@ package com.quirkshop.nuisancemaps.controller;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 import com.quirkshop.nuisancemaps.dto.FeatureCollectionDTO;
 import com.quirkshop.nuisancemaps.model.Data311;
@@ -43,46 +44,40 @@ public class Data311Controller {
     @CrossOrigin(origins = "${CORS_ORIGINS}")
     @GetMapping("/data311s.geojson")
     public FeatureCollectionDTO getIndexGeoJSON(
-            @RequestParam(name = "startDate", required = false) String startDate,
-            @RequestParam(name = "endDate", required = false) String endDate,
-            @RequestParam(name = "lat", required = false) String lat,
-            @RequestParam(name = "lng", required = false) String lng,
-            @RequestParam(name = "sw_lat", required = false) String sw_lat,
-            @RequestParam(name = "sw_lng", required = false) String sw_lng,
-            @RequestParam(name = "ne_lat", required = false) String ne_lat,
-            @RequestParam(name = "ne_lng", required = false) String ne_lng,
-            @RequestParam(name = "page", required = false) Integer page,
-            @RequestParam(name = "limit", required = false) Integer limit) {
+            @RequestParam(name = "startDate", required = false) Optional<String> startDate,
+            @RequestParam(name = "endDate", required = false) Optional<String> endDate,
+            @RequestParam(name = "lat", required = false, defaultValue = "30.2944") Optional<String> lat,
+            @RequestParam(name = "lng", required = false, defaultValue = "-97.7171") Optional<String> lng,
+            @RequestParam(name = "sw_lat", required = false) Optional<String> sw_lat,
+            @RequestParam(name = "sw_lng", required = false) Optional<String> sw_lng,
+            @RequestParam(name = "ne_lat", required = false) Optional<String> ne_lat,
+            @RequestParam(name = "ne_lng", required = false) Optional<String> ne_lng,
+            @RequestParam(name = "limit", required = false) Optional<Integer> limit) {
 
-        final int LIMIT = 10000;
+        final int MAX_LIMIT = 1000;
 
-        // defaults
-        int distance = 1;
-        double latitude = 30.2944;
-        double longitude = -97.7171;
-        double _sw_lat = latitude;
-        double _sw_lng = longitude;
-        double _ne_lat = latitude;
-        double _ne_lng = longitude;
-        LocalDateTime startDateTime = LocalDateTime.now().minusYears(1);
-        LocalDateTime endDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
 
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
-            startDateTime = LocalDateTime.parse(startDate + " 0:00", formatter);
-            endDateTime = LocalDateTime.parse(endDate + " 0:00", formatter);
-            latitude = Double.parseDouble(lat);
-            longitude = Double.parseDouble(lng);
-            _sw_lat = Double.parseDouble(sw_lat);
-            _sw_lng = Double.parseDouble(sw_lng);
-            _ne_lat = Double.parseDouble(ne_lat);
-            _ne_lng = Double.parseDouble(ne_lng);
-        } catch (Exception e) {
-            System.err.println("[Err] parse args " + e.getMessage());
-        }
+        LocalDateTime startDateTime = startDate
+                .map(date -> LocalDateTime.parse(startDate.get() + " 0:00", formatter))
+                .orElse(LocalDateTime.now().minusYears(1));
+
+        LocalDateTime endDateTime = endDate
+                .map(date -> LocalDateTime.parse(endDate.get() + " 0:00", formatter))
+                .orElse(LocalDateTime.now());
+
+        double _lat = lat.map(Double::parseDouble).get();
+        double _lng = lng.map(Double::parseDouble).get();
+
+        double _sw_lat = sw_lat.map(Double::parseDouble).orElse(_lat);
+        double _sw_lng = sw_lng.map(Double::parseDouble).orElse(_lng);
+        double _ne_lat = ne_lat.map(Double::parseDouble).orElse(_lat);
+        double _ne_lng = ne_lng.map(Double::parseDouble).orElse(_lng);
+
+        int _limit = limit.map(Integer::valueOf).orElse(MAX_LIMIT);
 
         return data311Repository.findAllByBoundsOrderByReportedAtDescGeoJSON(_sw_lat, _sw_lng, _ne_lat, _ne_lng,
-                startDateTime, endDateTime, Math.min(limit, LIMIT));
+                startDateTime, endDateTime, Math.min(_limit, MAX_LIMIT));
 
     }
 
