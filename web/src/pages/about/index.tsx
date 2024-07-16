1549ff9b0be517513c58d03279d0f2931f5932ec
Alan Verga <alan.verga@gmail.com>
2024-07-16 11:03:28 -0500

    Topic/pages/init (#62)
    * set navbar fixed, adjust ControlBar component padding
    
    * initial About page layout
    
    * add pages directory with About partials
    
    * add initial pages content
    
    * add tailwind typography package
    minor package updates
    
    * update pages partials with content and layout spacing

---
+++ web/src/pages/about/index.tsx
@@ -0,0 +1,35 @@
+import Description from "./_description";
+import Terms from "./_terms";
+import Privacy from "./_privacy";
+
+export default function About() {
+  return (
+    <div className="container mx-auto mt-16">
+      <div className="flex flex-col sm:flex-row py-1 justify-center">
+        <div id="menu" className="sm:mt-8 pr-4">
+          <ul className="fixed menu menu-xs menu-horizontal sm:menu-sm sm:menu-vertical bg-base-200 w-full sm:w-56">
+            <li>
+              <a href="#311CrimeMap">311 Crime Map</a>
+            </li>
+            <li>
+              <a href="#terms">Terms of Service</a>
+            </li>
+            <li>
+              <a href="#privacy">Privacy Policy</a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="content" className="sm:ml-60 px-4 mt-6 sm:-mt-2">
+          <Description />
+          <div className="divider"></div>
+
+          <Terms />
+          <div className="divider"></div>
+
+          <Privacy />
+        </div>
+      </div>
+    </div>
+  );
+}
