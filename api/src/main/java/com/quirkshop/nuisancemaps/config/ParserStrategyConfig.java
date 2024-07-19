569d4a9fd5eef2e640c98fcff4fca4ad6b29321a
vergeman <alan.verga@gmail.com>
2024-07-19 11:46:05 -0500

    add ParserStrategyConfig implementation with tests

--- api/src/main/java/com/quirkshop/nuisancemaps/config/ParserStrategyConfig.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/config/ParserStrategyConfig.java
@@ -5,12 +5,17 @@ import java.util.Map;
 import java.util.function.Function;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.quirkshop.nuisancemaps.WorkerApplication;
 @Configuration
 public class ParserStrategyConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(WorkerApplication.class);
 
     @Bean
     public Map<ParserStrategy, Function<JsonNode, String>> parsingFunctions() {
@@ -25,13 +30,31 @@ public class ParserStrategyConfig {
 
     public String LATITUDE_311_DALLAS(JsonNode item) {
         // {..., "lat_location": (32.77937339624264000,-96.85251201839743000), ...}
-        String latitude = item.at("/lat_location").asText();
+        String latitude = null;
+
+        try {
+            String text = item.at("/lat_location").asText();
+            String coordinates = text.replaceAll("[()]", "");
+            latitude = coordinates.split(",")[0];
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        }
+
         return latitude;
     }
 
     public String LONGITUDE_311_DALLAS(JsonNode item) {
         // {..., "lat_location": (32.77937339624264000,-96.85251201839743000), ...}
-        String longitude = item.at("/lat_location").asText();
+        String longitude = null;
+
+        try {
+            String text = item.at("/lat_location").asText();
+            String coordinates = text.replaceAll("[()]", "");
+            longitude = coordinates.split(",")[1];
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        }
+
         return longitude;
     }
 }
