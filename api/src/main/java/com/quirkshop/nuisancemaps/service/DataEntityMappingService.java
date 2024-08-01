004acac803b1e6e9928374ec2cf1e05d15ff856b
vergeman <alan.verga@gmail.com>
2024-08-01 17:52:05 -0500

    initial CSVDataParser and DataEntityMappingService:buildDataEntity implementation

--- api/src/main/java/com/quirkshop/nuisancemaps/service/DataEntityMappingService.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/service/DataEntityMappingService.java
@@ -1,69 +1,56 @@
 package com.quirkshop.nuisancemaps.service;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.function.Function;
 
-import org.apache.commons.lang3.StringUtils;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.geom.PrecisionModel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Iterables;
-import com.quirkshop.nuisancemaps.model.IDataEntity;
-import com.quirkshop.nuisancemaps.model.Locale;
-import com.quirkshop.nuisancemaps.model.Mapping;
-import com.quirkshop.nuisancemaps.model.MappingField;
 import com.quirkshop.nuisancemaps.config.MissingCategoryException;
 import com.quirkshop.nuisancemaps.config.MissingCoordinateException;
 import com.quirkshop.nuisancemaps.config.MissingReportCategoryException;
 import com.quirkshop.nuisancemaps.config.ParserStrategy;
 import com.quirkshop.nuisancemaps.model.Category;
-import com.quirkshop.nuisancemaps.model.Data311;
-import com.quirkshop.nuisancemaps.model.DataCrime;
-import com.quirkshop.nuisancemaps.model.DataError;
-import com.quirkshop.nuisancemaps.model.DataJob;
-import com.quirkshop.nuisancemaps.model.DataJobStatus;
+import com.quirkshop.nuisancemaps.model.IDataEntity;
+import com.quirkshop.nuisancemaps.model.Locale;
+import com.quirkshop.nuisancemaps.model.Mapping;
+import com.quirkshop.nuisancemaps.model.MappingField;
 import com.quirkshop.nuisancemaps.model.Source;
-import com.quirkshop.nuisancemaps.repository.Data311Repository;
-import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
-import com.quirkshop.nuisancemaps.repository.DataErrorRepository;
-import com.quirkshop.nuisancemaps.repository.IDataEntityRepository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataEntityMappingService {
-    private final Map<ParserStrategy, Function<JsonNode, String>> parsingFunctions;
+    private final Map<ParserStrategy, Function<JsonNode, String>> parsingFunctionsJSON;
+    private final Map<ParserStrategy, Function<Map<String, String>, String>> parsingFunctionsMap;
 
     @Autowired
-    public DataEntityMappingService(Map<ParserStrategy, Function<JsonNode, String>> parsingFunctions) {
-        this.parsingFunctions = parsingFunctions;
+    public DataEntityMappingService(Map<ParserStrategy, Function<JsonNode, String>> parsingFunctionsJSON,
+            Map<ParserStrategy, Function<Map<String, String>, String>> parsingFunctionsMap) {
+        this.parsingFunctionsJSON = parsingFunctionsJSON;
+        this.parsingFunctionsMap = parsingFunctionsMap;
     }
 
     @Autowired
     private TextCategoryService textCategoryService;
 
     public String parseNode(JsonNode item, ParserStrategy strategy) {
-        Function<JsonNode, String> parser = parsingFunctions.get(strategy);
+        Function<JsonNode, String> parser = parsingFunctionsJSON.get(strategy);
         if (parser != null) {
             return parser.apply(item);
+        }
+        return null;
+    }
+
+    public String parseRow(Map<String, String> row, ParserStrategy strategy) {
+        Function<Map<String, String>, String> parser = parsingFunctionsMap.get(strategy);
+        if (parser != null) {
+            return parser.apply(row);
         }
         return null;
     }
@@ -72,7 +59,7 @@ public class DataEntityMappingService {
      * hierarchy of parse methods
      * 1. vanilla string (simple getter method returns String; not MappingField)
      * 2. ParserStrategy method: if this is defined, prioritize it's use
-     * 3. default MappingField pointer (json parse 'path expression')
+     * 3. default MappingField pointer (json pointer)
      */
     public String parseEntity(Function<Mapping, ?> mapper, Source source, JsonNode item)
             throws NoSuchMethodException, SecurityException {
@@ -83,7 +70,7 @@ public class DataEntityMappingService {
         Object mappedValue = mapper.apply(source.getMapping());
         String value = null;
 
-        // Vanilla String
+        // Vanilla String (e.g. orderKey: ":id")
         if (returnType == String.class) {
             return (String) mappedValue;
         }
@@ -104,11 +91,115 @@ public class DataEntityMappingService {
         return value;
     }
 
+    public String parseEntity(Function<Mapping, ?> mapper, Source source, Map<String, String> row)
+            throws NoSuchMethodException, SecurityException {
+        Method method = mapper.getClass().getMethod("apply", Object.class);
+        Class<?> returnType = method.getReturnType();
+
+        Object mappedValue = mapper.apply(source.getMapping());
+        String value = null;
+
+        // Vanilla String (e.g. orderKey: ":id")
+        if (returnType == String.class) {
+            return (String) mappedValue;
+        }
+
+        // Parsing Strategy method if exists, otherwise use Pointer expression
+        MappingField result = (MappingField) mappedValue;
+
+        if (result == null)
+            return null;
+
+        if (result.getParsingStrategy() != null) {
+            ParserStrategy strategy = ParserStrategy.valueOf(result.getParsingStrategy());
+            value = parseRow(row, strategy);
+        } else {
+            value = row.get(result.getField());
+        }
+
+        return value;
+
+    }
+
+    public IDataEntity buildDataEntity(Class<? extends IDataEntity> dataEntityClass, Source source,
+            Map<String, String> row,
+            GeometryFactory geometryFactory)
+            throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException,
+            MissingCategoryException, MissingReportCategoryException, MissingCoordinateException {
+
+        String report_num = parseEntity(Mapping::getReportNum, source, row);
+        String reportCategory = parseEntity(Mapping::getReportCategory, source, row);
+        String description = parseEntity(Mapping::getDescription, source, row);
+        String location = parseEntity(Mapping::getLocation, source, row);
+
+        String lat = parseEntity(Mapping::getLatitude, source, row);
+        String lng = parseEntity(Mapping::getLongitude, source, row);
+
+        String reported_at1 = parseEntity(Mapping::getReportedAt, source, row);
+        String reported_at2 = parseEntity(Mapping::getReportedAt2, source, row);
+
+        Double latitude = (lat == null || lat.isEmpty()) ? null : Double.parseDouble(lat);
+        Double longitude = (lng == null || lng.isEmpty()) ? null : Double.parseDouble(lng);
+        Coordinate coordinate = null;
+        Point point = null;
+
+        // validate
+
+        if (reportCategory == null || reportCategory.isEmpty()) {
+            String errString = String.format(
+                    "Missing reportCategory | dataType: %s | id: %s | %s | sourceURL: %s",
+                    source.getCategory(), source.getSourceConfigId(), source.getDescription(), source.getUrl());
+            throw new MissingReportCategoryException(errString);
+        }
+
+        if (latitude != null && longitude != null) {
+            // GeoJSON/WKT is long, lat (order is "reversed").
+            coordinate = new Coordinate(longitude, latitude);
+            point = geometryFactory.createPoint(coordinate);
+        } else {
+            String errString = String.format(
+                    "Missing coordinates: (lat: %s, lng: %s) | dataType: %s | id: %s | %s | sourceURL: %s", lat, lng,
+                    source.getCategory(), source.getSourceConfigId(), source.getDescription(), source.getUrl());
+            throw new MissingCoordinateException(errString);
+        }
+
+        // categories clarification
+        // source.category: crime / 311 / etc
+        // dataEntity.report_category: data report instance from raw data
+        // Category: our created, labeled categories
+        Category orgCategory = textCategoryService.lookupCategory(source.getCategory(), reportCategory);
+        if (orgCategory == null) {
+            String errString = String.format("Missing category: %s | dataType: %s | id: %s | %s | sourceURL: %s",
+                    reportCategory,
+                    source.getCategory(), source.getSourceConfigId(), source.getDescription(), source.getUrl());
+            throw new MissingCategoryException(errString);
+        }
+        // set values
+
+        LocalDateTime reported_at = reported_at1.isEmpty() ? LocalDateTime.parse(reported_at2)
+                : LocalDateTime.parse(reported_at1);
+
+        IDataEntity dataEntity = dataEntityClass.getConstructor(Source.class).newInstance(source);
+
+        dataEntity.setReportNum(report_num);
+        dataEntity.setReportCategory(reportCategory);
+        dataEntity.setDescription(description);
+        dataEntity.setLocation(location);
+        dataEntity.setOrgCategory(orgCategory);
+        dataEntity.setLatitude(latitude);
+        dataEntity.setLongitude(longitude);
+        dataEntity.setPoint(point);
+        dataEntity.setReportedAt(reported_at);
+        dataEntity.setUpdatedAt(LocalDateTime.now());
+
+        return dataEntity;
+
+    }
+
     public IDataEntity buildDataEntity(Class<? extends IDataEntity> dataEntityClass, Source source, JsonNode item,
             GeometryFactory geometryFactory)
             throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException,
             MissingCategoryException, MissingReportCategoryException, MissingCoordinateException {
-        Locale locale = source.getLocale();
 
         String report_num = parseEntity(Mapping::getReportNum, source, item);
         String reportCategory = parseEntity(Mapping::getReportCategory, source, item);
@@ -150,7 +241,8 @@ public class DataEntityMappingService {
         // Category: our created, labeled categories
         Category orgCategory = textCategoryService.lookupCategory(source.getCategory(), reportCategory);
         if (orgCategory == null) {
-            String errString = String.format("Missing category: %s | dataType: %s | id: %s | %s | sourceURL: %s", reportCategory,
+            String errString = String.format("Missing category: %s | dataType: %s | id: %s | %s | sourceURL: %s",
+                    reportCategory,
                     source.getCategory(), source.getSourceConfigId(), source.getDescription(), source.getUrl());
             throw new MissingCategoryException(errString);
         }
---
+++ api/src/test/java/com/quirkshop/nuisancemaps/service/dataparser/CSVDataParserTest.java
@@ -0,0 +1,136 @@
+package com.quirkshop.nuisancemaps.service.dataparser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.quirkshop.nuisancemaps.NuisancemapsApplication;
+import com.quirkshop.nuisancemaps.model.Category;
+import com.quirkshop.nuisancemaps.model.DataJob;
+import com.quirkshop.nuisancemaps.model.Locale;
+import com.quirkshop.nuisancemaps.model.Source;
+import com.quirkshop.nuisancemaps.model.TextCategory;
+import com.quirkshop.nuisancemaps.repository.CategoryRepository;
+import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
+import com.quirkshop.nuisancemaps.repository.DataJobRepository;
+import com.quirkshop.nuisancemaps.repository.LocaleRepository;
+import com.quirkshop.nuisancemaps.repository.MappingRepository;
+import com.quirkshop.nuisancemaps.repository.SourceRepository;
+import com.quirkshop.nuisancemaps.repository.TextCategoryRepository;
+import com.quirkshop.nuisancemaps.util.ParseCounter;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(classes = NuisancemapsApplication.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CSVDataParserTest {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private LocaleRepository localeRepository;
+
+    @Autowired
+    private MappingRepository mappingRepository;
+
+    @Autowired
+    private SourceRepository sourceRepository;
+
+    @Autowired
+    private DataJobRepository dataJobRepository;
+
+    @Autowired
+    private DataCrimeRepository dataCrimeRepository;
+
+    @Autowired
+    private TextCategoryRepository textCategoryRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CSVDataParser csvDataParser;
+
+    private List<Source> sources;
+
+    @BeforeAll
+    public void setUpOnce() throws IOException {
+        // Source
+        ObjectMapper objectMapper = new ObjectMapper();
+        File sourceJSON = resourceLoader.getResource("classpath:data/source_config.json").getFile();
+        sources = objectMapper.readValue(sourceJSON, new TypeReference<List<Source>>() {
+        });
+        for (Source s : sources) {
+            Locale locale = new Locale();
+            localeRepository.save(locale);
+            s.setLocale(locale);
+            mappingRepository.save(s.getMapping());
+            sourceRepository.save(s);
+        }
+    }
+
+    @AfterAll
+    public void tearDown() throws IOException {
+        sourceRepository.deleteAll();
+        mappingRepository.deleteAll();
+        localeRepository.deleteAll();
+        textCategoryRepository.deleteAll();
+        categoryRepository.deleteAll();
+    }
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        // require textCategory mapping to exist before successful save
+        // otherwise will throw MissingCategoryException and skip
+        Category cat = new Category("crime", "Public Order", 0, null);
+        categoryRepository.save(cat);
+
+        ArrayList<TextCategory> textCategories = new ArrayList<TextCategory>();
+        textCategories.add(new TextCategory("crime", "SEX CRIMES", cat));
+        textCategories.add(new TextCategory("crime", "HARRASSMENT 2", cat));
+        textCategories.add(new TextCategory("crime", "PETIT LARCENY", cat));
+        textCategories.add(new TextCategory("crime", "GRAND LARCENY", cat));
+        textCategories.add(new TextCategory("crime", "MURDER & NON-NEGL. MANSLAUGHTER", cat));
+        textCategories.add(new TextCategory("crime", "CRIMINAL MISCHIEF & RELATED OF", cat));
+        textCategories.add(new TextCategory("crime", "GRAND LARCENY OF MOTOR VEHICLE", cat));
+        textCategories.add(new TextCategory("crime", "OFF. AGNST PUB ORD SENSBLTY &", cat));
+
+        textCategoryRepository.saveAll(textCategories);
+    }
+
+    @Test
+    @Transactional
+    public void parse() throws IOException {
+
+        Resource jsonResource = resourceLoader.getResource("classpath:data/crime-nyc.csv");
+        InputStream inputstream = jsonResource.getInputStream();
+        ParseCounter parseCounter = new ParseCounter();
+
+        Source s = sourceRepository.findOneBySourceConfigId(12);
+        DataJob d = new DataJob(LocalDateTime.now(), s, 1000, 100, "CMPLNT_NUM");
+        dataJobRepository.save(d);
+
+        assertThat(dataCrimeRepository.count()).isEqualTo(0);
+
+        csvDataParser.parse(d, inputstream, parseCounter);
+
+        assertThat(dataCrimeRepository.count()).isEqualTo(9);
+    }
+}
