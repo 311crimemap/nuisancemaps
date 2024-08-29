b3536fc0f223980a532ba3e4a79d2ee22ecee8e2
vergeman <alan.verga@gmail.com>
2024-08-29 07:52:32 -0500

    MissingCategoryException: expand to reportCategory to exception 'payload'

--- api/src/main/java/com/quirkshop/nuisancemaps/config/MissingCategoryException.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/config/MissingCategoryException.java
@@ -1,7 +1,19 @@
 package com.quirkshop.nuisancemaps.config;
 
 public class MissingCategoryException extends Exception {
+
+    private String reportCategory;
+
     public MissingCategoryException(String errorMessage) {
         super(errorMessage);
+    }
+
+    public MissingCategoryException(String reportCategory, String errorMessage) {
+        super(errorMessage);
+        this.reportCategory = reportCategory;
+    }
+
+    public String getReportCategory() {
+        return reportCategory;
     }
 }
