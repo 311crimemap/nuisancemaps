656f82e3dca3fb9a8bfc4310b9a6ec31efc9db76
Alan Verga <alan.verga@gmail.com>
2024-08-29 10:37:21 -0500

    Topic/locale/attribution and content page display (#92)
    * Locale model add city, state, attribution, and enabled flag
    Update LocaleDTO
    add migration
    
    * LocaleController: add patch update endpoint for entity update
    update locale_config.json with new fields

--- api/src/main/java/com/quirkshop/nuisancemaps/controller/LocaleController.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/controller/LocaleController.java
@@ -7,6 +7,7 @@ import org.locationtech.jts.geom.PrecisionModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,6 +46,9 @@ public class LocaleController {
     @Autowired
     SourceLoaderService sourceLoaderService;
 
+    private final int SRID = 4326;
+    private final GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING),
+            SRID);
     private static final Logger log = LoggerFactory.getLogger(NuisancemapsApplication.class);
 
     @GetMapping("/locales/{id}")
@@ -101,6 +106,48 @@ public class LocaleController {
             return ResponseEntity.badRequest().body(jSendDTO);
         }
         return ResponseEntity.ok().body(jSendDTO);
+    }
+
+    @PatchMapping("/locales/{id}")
+    public ResponseEntity<?> update(@PathVariable(value = "id") final int id,
+            @RequestBody LocaleDTO localeDTO) {
+        JSendDTO jSendDTO;
+        Locale locale = null;
+        try {
+            locale = localeRepository.findById(id).orElse(null);
+
+            if (locale != null) {
+                Field[] fields = LocaleDTO.class.getDeclaredFields();
+
+                for (Field localeDTOField : fields) {
+                    localeDTOField.setAccessible(true);
+                    Object value = localeDTOField.get(localeDTO);
+
+                    if (value != null) {
+                        Field localeField = Locale.class.getDeclaredField(localeDTOField.getName());
+                        localeField.setAccessible(true);
+
+                        if (localeDTOField.getName().equals("location")) {
+                            double x = localeDTO.getLocation()[0];
+                            double y = localeDTO.getLocation()[1];
+                            value = geometryFactory.createPoint(new Coordinate(x, y));
+                        }
+
+                        localeField.set(locale, value);
+                    }
+                }
+                locale = localeRepository.save(locale);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            jSendDTO = new JSendDTO("error", e.getMessage());
+            return ResponseEntity.badRequest().body(jSendDTO);
+        }
+
+        jSendDTO = new JSendDTO("success", locale.toDTO());
+        return ResponseEntity.status(HttpStatus.OK).body(jSendDTO);
+
     }
 
 }
