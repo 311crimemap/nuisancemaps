1fea17b4359e7f465e547f58bee8322db631c6f6
vergeman <alan.verga@gmail.com>
2024-09-03 14:57:15 -0500

    formatting for cities pages

--- web/src/pages/cities/_state_cities.tsx
+++ web/src/pages/cities/_state_cities.tsx
@@ -2,33 +2,25 @@ import { Link } from "react-router-dom";
 import { slugify } from "../../Util";
 
 export default function StateCities({ states }) {
-
-    return (
-
-
-        Object.keys(states).sort().map((state) => {
-            return (
-                <article key={state} className="prose">
-                    <h4>{state}</h4>
-                    <ul>
-                        {
-                            Object.values(states[state]).sort().map(val => {
-                                return (
-                                    <li key={val.city}>
-                                        <Link to={`/${slugify(val.city)}`} >{val.city} </Link>
-                                    </li>
-                                )
-
-                            })
-                        }
-
-                    </ul>
-                    <div className="divider"></div>
-                </article>
-
-            )
-        })
-
-
-    )
+  return Object.keys(states)
+    .sort()
+    .map((state) => {
+      return (
+        <article key={state} className="prose">
+          <h4>{state}</h4>
+          <ul>
+            {Object.values(states[state])
+              .sort()
+              .map((val) => {
+                return (
+                  <li key={val.city}>
+                    <Link to={`/${slugify(val.city)}`}>{val.city} </Link>
+                  </li>
+                );
+              })}
+          </ul>
+          <div className="divider"></div>
+        </article>
+      );
+    });
 }
--- web/src/pages/cities/_states.tsx
+++ web/src/pages/cities/_states.tsx
@@ -1,24 +1,18 @@
+export default function States({ states }) {
+  return (
+    <ul className="fixed menu menu-xs menu-horizontal sm:menu-sm sm:menu-vertical bg-base-200 w-full sm:w-56">
+      <li>
+        <a href="#311CrimeMap">States</a>
+      </li>
 
-export default function States({states}) {
-
-    return (
-        <ul className="fixed menu menu-xs menu-horizontal sm:menu-sm sm:menu-vertical bg-base-200 w-full sm:w-56">
-            <li>
-                <a href="#311CrimeMap">
-                    States
-                </a>
-            </li>
-
-            <hr/>
-            {states.map(state => {
-                return (
-                <li key={state}>
-                    <a href={`#${state}`}>{state}</a>
-                </li>
-                )
-
-            })}
-
-        </ul>
-    );
+      <hr />
+      {states.map((state) => {
+        return (
+          <li key={state}>
+            <a href={`#${state}`}>{state}</a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
