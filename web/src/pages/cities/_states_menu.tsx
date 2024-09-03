d08d1ad04016c206aac87c405e6d9607fde296c7
vergeman <alan.verga@gmail.com>
2024-09-03 16:19:30 -0500

    states menu mobile view butterbar only populous states

--- web/src/pages/cities/_states_menu.tsx
+++ web/src/pages/cities/_states_menu.tsx
@@ -1,12 +1,26 @@
 import { slugify } from "../../Util";
 
 export default function StatesMenu({ states }) {
+  // reduce to most populous states
+  const mobileStatesMap = {
+    "California": true,
+    "Florida": true,
+    "Illinois": true,
+    "New York": true,
+    "Pennsylvania": true,
+    "Texas": true,
+  };
+
+
+
   return (
     <ul className="fixed menu menu-xs menu-horizontal sm:menu-sm sm:menu-vertical bg-base-200 w-full sm:w-56">
       <hr />
       {states.sort().map((state) => {
+        const hideMobile = !mobileStatesMap[state];
+
         return (
-          <li key={state}>
+          <li key={state} className={hideMobile ? "hidden sm:block" : ""}>
             <a href={`#${slugify(state)}`}>{state}</a>
           </li>
         );
