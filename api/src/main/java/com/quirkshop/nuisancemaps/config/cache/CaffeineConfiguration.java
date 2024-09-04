0a5df23a0ea2738d431600c21ffb83b7bfcd9094
vergeman <alan.verga@gmail.com>
2024-09-04 17:43:04 -0500

    CaffeineConfiguration: configure caffeineConfig instance (1 hr expiry, max size 100) and assign to cacheManager

---
+++ api/src/main/java/com/quirkshop/nuisancemaps/config/cache/CaffeineConfiguration.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.TimeUnit;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CaffeineConfiguration {
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(60, TimeUnit.MINUTES)
+                .maximumSize(100);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
