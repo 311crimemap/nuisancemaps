073fd6d5dc8429b7ea3ced195b9ad816c7dc9051
vergeman <alan.verga@gmail.com>
2024-09-09 18:00:36 -0500

    create-secrets.sh fix for missing '\'

--- ops/deploy/create-secrets.sh
+++ ops/deploy/create-secrets.sh
@@ -32,6 +32,6 @@ kubectl create secret generic pgbackrest-secrets \
 kubectl delete secret api-secrets --ignore-not-found=true
 kubectl create secret generic api-secrets \
         --from-literal=ADMIN_API_KEY=$ADMIN_API_KEY \
-        --from-literal=VITE_MAPTILER_API_KEY=$VITE_MAPTILER_API_KEY
+        --from-literal=VITE_MAPTILER_API_KEY=$VITE_MAPTILER_API_KEY \
         --from-literal=GEOAPIFY_API_KEY=$GEOAPIFY_API_KEY
 
