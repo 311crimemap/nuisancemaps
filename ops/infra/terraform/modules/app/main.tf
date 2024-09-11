043de6d270e748906895100794d74683ce07ef80
Alan Verga <alan.verga@gmail.com>
2024-09-10 19:24:41 -0500

    Topic/deploy/traefik (#106)
    * add docker-compose.yml traefik configuration to reverse proxy api service
    
    * traefik: enable compression middleware
    README.md: add Traefik notes
    
    * k3s: add default-compress-middleware to traefik ingress
    
    * create-secrets.sh fix for missing '\'
    
    * README.md: fix formatting for certs explanation
    
    * README.md: add k3s middleware notes
    
    * revised ingress configuration for api and staging api env
    
    * k3s updated config to modify traefik default with nodeSelector and tolerance
    
    * ops: README.md: add traefik notes

--- ops/infra/terraform/modules/app/main.tf
+++ ops/infra/terraform/modules/app/main.tf
@@ -54,6 +54,7 @@ resource "hcloud_server" "app" {
       "class_instance": "${var.class_id}-${count.index}"
 
       # deploy k3s
+      # label creates ansible group: label_node_server / label_node_agent
       "node" :  var.k3s_server && count.index == 0 ? "server" : "agent"
     })
 
