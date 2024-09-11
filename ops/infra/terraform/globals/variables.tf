adb6aba43e719267bd99bc49966c4203c7cf16fe
vergeman <alan.verga@gmail.com>
2024-09-11 09:14:46 -0500

    [ops] terraform add cloudflare whitelist ip range to firewall module

--- ops/infra/terraform/globals/variables.tf
+++ ops/infra/terraform/globals/variables.tf
@@ -2,5 +2,10 @@
 
 variable hcloud_token {
   type = string
-  description = "hetzner token passed in via module level"
+  description = "hetzner token passed (TF_VAR_ env variable)"
+}
+
+variable cloudflare_api_token {
+  type = string
+  description = "Cloudflare API Token (TF_VAR_ env variable)"
 }
--- ops/infra/terraform/modules/firewall/versions.tf
+++ ops/infra/terraform/modules/firewall/versions.tf
@@ -4,5 +4,9 @@ terraform {
       source = "hetznercloud/hcloud"
       version = ">= 1.44.1"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 }
