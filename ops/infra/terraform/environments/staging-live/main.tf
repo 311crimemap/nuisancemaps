3749ba032bb4070efae57fff4d28e217c37a4439
Alan Verga <alan.verga@gmail.com>
2024-10-31 15:48:04 -0500

    Topic/primary replica db/k3s (#124)
    * add helm installer script
    
    * add initial values.yml for postgresql-ha helm chart
    
    * add pgpool-configmap
    
    * add postgresql-configmap, pgbackrest-configmap
    rename references from postgresql-config, pgbackrest-config -> configmap
    
    * db-service.yml: refactor out headless postgresql-service to wrapper db service
    (helm handles postgresql headless service)
    
    * create-secrets.sh: add missing secrets and keys
    values.yml: update postgresql-secrets name
    
    * values.yml: add shared-data-volume for /mnt directory
    add busybox init-container for permissions and mkdir
    
    * remove postgresql-statefulset.yml (replaced with helm)
    
    * README.md: add pg_dump / psql notes
    
    * update postgres.conf options for import experimentation
    
    * values.yml: add resources block to prevent OOM crash with psql import
    add affinity rules to schedule pod to node
    
    * helm values.yml increase dshm
    
    * [ops] add api resource limits
    
    * [ops] pgpool-configmap to query split equally b/w primary / replica
    
    * [ops] update terraform staging infra config
    
    * [ops] values.yml: revised postgres statefulset pin primary to db-0
    
    * [api] application.properties increase connection size to 20
    
    * README.md: minor nodes on uninstall

--- ops/infra/terraform/environments/staging-live/main.tf
+++ ops/infra/terraform/environments/staging-live/main.tf
@@ -61,14 +61,14 @@ module "app-servers" {
     network_zone: "us-west"
   }
 
-  server_type = "cpx11"
+  server_type = "cpx21"
   server_count = 1
   ssh_key_name = var.ssh_key_name
 
   image_name = "name=packer_base_311crimemap_1.0"
 
   additional_labels = {}
-  k3s_server = true
+  k3s_server = false
 
   network_id   = module.network.hcloud_network_id
   network_subnet_id = module.network.hcloud_network_subnet_id
@@ -119,14 +119,14 @@ module "db-server" {
     network_zone: "us-west"
   }
 
-  server_type = "cpx11"
-  server_count = 1
+  server_type = "cpx21"
+  server_count = 2
   ssh_key_name = var.ssh_key_name
 
   image_name = "name=packer_base_311crimemap_1.0"
 
   additional_labels = {}
-  k3s_server = false
+  k3s_server = true
 
   network_id   = module.network.hcloud_network_id
   network_subnet_id = module.network.hcloud_network_subnet_id
