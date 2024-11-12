8c7ca453594dcf9d6e8eb88779c35563a15d672c
vergeman <alan.verga@gmail.com>
2024-11-12 17:09:06 -0600

    Data controllers: add log output

--- api/src/main/java/com/quirkshop/nuisancemaps/controller/Data311Controller.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/controller/Data311Controller.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.quirkshop.nuisancemaps.NuisancemapsApplication;
+
 import com.quirkshop.nuisancemaps.dto.FeatureCollectionDTO;
 import com.quirkshop.nuisancemaps.model.Data311;
 import com.quirkshop.nuisancemaps.repository.Data311Repository;
@@ -31,6 +35,10 @@ public class Data311Controller {
     Data311Repository data311Repository;
 
     private static final int MAX_LIMIT = Integer.parseInt(System.getenv("VITE_MAX_DATA_RECORDS"));
+
+    private int count = 0;
+
+    private static final Logger log = LoggerFactory.getLogger(NuisancemapsApplication.class);
 
     @CrossOrigin(origins = "${CORS_ORIGINS}")
     @GetMapping("/data311s")
@@ -81,6 +89,10 @@ public class Data311Controller {
         double _sw_lng = sw_lng.map(Double::parseDouble).orElse(_lng);
         double _ne_lat = ne_lat.map(Double::parseDouble).orElse(_lat);
         double _ne_lng = ne_lng.map(Double::parseDouble).orElse(_lng);
+
+        count++;
+        String logStr = String.format("Data311 %d: %s %s %s %s: ", count, _sw_lat, _sw_lng, _ne_lat, _ne_lng);
+        log.info(logStr);
 
         return data311Repository.findAllByBoundsOrderByReportedAtDescGeoJSON(_sw_lat, _sw_lng, _ne_lat, _ne_lng,
                 startDateTime, endDateTime, MAX_LIMIT);
--- api/src/main/java/com/quirkshop/nuisancemaps/controller/DataCrimeController.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/controller/DataCrimeController.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.quirkshop.nuisancemaps.NuisancemapsApplication;
+
 import com.quirkshop.nuisancemaps.dto.FeatureCollectionDTO;
 import com.quirkshop.nuisancemaps.model.DataCrime;
 import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
@@ -24,6 +28,10 @@ public class DataCrimeController {
     DataCrimeRepository dataCrimeRepository;
 
     private static final int MAX_LIMIT = Integer.parseInt(System.getenv("VITE_MAX_DATA_RECORDS"));
+
+    private int count = 0;
+
+    private static final Logger log = LoggerFactory.getLogger(NuisancemapsApplication.class);
 
     @CrossOrigin(origins = "${CORS_ORIGINS}")
     @GetMapping("/datacrimes")
@@ -74,6 +82,10 @@ public class DataCrimeController {
         double _sw_lng = sw_lng.map(Double::parseDouble).orElse(_lng);
         double _ne_lat = ne_lat.map(Double::parseDouble).orElse(_lat);
         double _ne_lng = ne_lng.map(Double::parseDouble).orElse(_lng);
+
+        count++;
+        String logStr = String.format("DataCrime %d: %s %s %s %s: ", count, _sw_lat, _sw_lng, _ne_lat, _ne_lng);
+        log.info(logStr);
 
         return dataCrimeRepository.findAllByBoundsOrderByReportedAtDescGeoJSON(_sw_lat, _sw_lng, _ne_lat, _ne_lng,
                 startDateTime, endDateTime, MAX_LIMIT);
