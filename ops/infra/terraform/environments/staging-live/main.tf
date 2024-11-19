04a4e496b8011bf836034b297729a409e735b09f
vergeman <alan.verga@gmail.com>
2024-11-19 09:56:29 -0600

    add cntrl module for k3s server(s)

--- ops/infra/terraform/environments/staging-live/main.tf
+++ ops/infra/terraform/environments/staging-live/main.tf
@@ -46,6 +46,39 @@ module "network" {
 
 }
 
+#
+# k3s control server
+# metrics (e.g. grafana) and other misc
+#
+module "control-servers" {
+  source = "../../modules/app"
+
+  # define variables to pass into this module
+  product = var.product
+  env = var.env
+  env_group = var.env_group
+  org_id = "web"
+  class_id = "cntrl"
+
+  location_zone = {
+    location: "hil",
+    network_zone: "us-west"
+  }
+
+  server_type = "cpx21"
+  server_count = 1
+  ssh_key_name = var.ssh_key_name
+
+  image_name = "name=packer_base_311crimemap_1.0"
+
+  additional_labels = {}
+  k3s_server = true
+
+  network_id   = module.network.hcloud_network_id
+  network_subnet_id = module.network.hcloud_network_subnet_id
+  firewall_id = module.firewall.hcloud_firewall_id
+}
+
 module "app-servers" {
   source = "../../modules/app"
 
@@ -104,7 +137,7 @@ module "worker-servers" {
   firewall_id = module.firewall.hcloud_firewall_id
 }
 
-module "db-server" {
+module "db-servers" {
   source = "../../modules/db"
 
   # define variables to pass into this module
@@ -132,7 +165,7 @@ module "db-server" {
   image_name = "name=packer_base_311crimemap_1.0"
 
   additional_labels = {}
-  k3s_server = true
+  k3s_server = false
 
   network_id   = module.network.hcloud_network_id
   network_subnet_id = module.network.hcloud_network_subnet_id
