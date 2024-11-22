746fbd99e20ecd947aa6ddd096a6b4168535e8b1
vergeman <alan.verga@gmail.com>
2024-11-22 13:11:14 -0600

    Data311/DataCrime controller: required lat/lng params with more robust error handling remove deprecated index action

--- api/src/main/java/com/quirkshop/nuisancemaps/controller/DataCrimeController.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/controller/DataCrimeController.java
@@ -2,21 +2,19 @@ package com.quirkshop.nuisancemaps.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Optional;
+
+import com.quirkshop.nuisancemaps.NuisancemapsApplication;
+import com.quirkshop.nuisancemaps.dto.FeatureCollectionDTO;
+import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
+import com.quirkshop.nuisancemaps.service.DataCrimeService;
+import com.quirkshop.nuisancemaps.util.DataParamValidator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.quirkshop.nuisancemaps.NuisancemapsApplication;
-
-import com.quirkshop.nuisancemaps.dto.FeatureCollectionDTO;
-import com.quirkshop.nuisancemaps.model.DataCrime;
-import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
-import com.quirkshop.nuisancemaps.service.DataCrimeService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,62 +36,49 @@ public class DataCrimeController {
     private static final Logger log = LoggerFactory.getLogger(NuisancemapsApplication.class);
 
     @CrossOrigin(origins = "${CORS_ORIGINS}")
-    @GetMapping("/datacrimes")
-    public List<DataCrime> getIndex(
-            @RequestParam(name = "page", required = false) Integer page,
-            @RequestParam(name = "limit", required = false) Integer limit) {
-
-        final int LIMIT = 1000;
-
-        if (page != null && limit != null) {
-            return dataCrimeRepository.findAllByOrderByReportedAtDesc(PageRequest.of(page, limit));
-        } else if (page != null) {
-            return dataCrimeRepository.findAllByOrderByReportedAtDesc(PageRequest.of(page, LIMIT));
-        } else if (limit != null) {
-            return dataCrimeRepository.findAllByOrderByReportedAtDesc(PageRequest.of(0, limit));
-        }
-
-        return dataCrimeRepository.findAllByOrderByReportedAtDesc(PageRequest.of(0, LIMIT));
-    }
-
-    @CrossOrigin(origins = "${CORS_ORIGINS}")
     @GetMapping("/datacrimes.geojson")
     @Cacheable(value = "dataCrimeControllerCache", key = "#startDate + '-' + #endDate + '-' + #sw_lat + '-' + #sw_lng + '-' + #ne_lat + '-' + #ne_lng")
-    public FeatureCollectionDTO getIndexGeoJSON(
-            @RequestParam(name = "startDate", required = false) Optional<String> startDate,
-            @RequestParam(name = "endDate", required = false) Optional<String> endDate,
-            @RequestParam(name = "lat", required = false, defaultValue = "30.2944") Optional<String> lat,
-            @RequestParam(name = "lng", required = false, defaultValue = "-97.7171") Optional<String> lng,
-            @RequestParam(name = "sw_lat", required = false) Optional<String> sw_lat,
-            @RequestParam(name = "sw_lng", required = false) Optional<String> sw_lng,
-            @RequestParam(name = "ne_lat", required = false) Optional<String> ne_lat,
-            @RequestParam(name = "ne_lng", required = false) Optional<String> ne_lng) {
+    public ResponseEntity<?> getIndexGeoJSON(
+            @RequestParam(name = "startDate") Optional<String> startDate,
+            @RequestParam(name = "endDate") Optional<String> endDate,
+            @RequestParam(name = "sw_lat", required = true) String sw_lat,
+            @RequestParam(name = "sw_lng", required = true) String sw_lng,
+            @RequestParam(name = "ne_lat", required = true) String ne_lat,
+            @RequestParam(name = "ne_lng", required = true) String ne_lng) {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
+        try {
 
-        LocalDateTime startDateTime = startDate
-                .map(date -> LocalDateTime.parse(startDate.get() + " 0:00", formatter))
-                .orElse(LocalDateTime.now().minusYears(1));
+            double _sw_lat = Double.parseDouble(sw_lat);
+            double _sw_lng = Double.parseDouble(sw_lng);
+            double _ne_lat = Double.parseDouble(ne_lat);
+            double _ne_lng = Double.parseDouble(ne_lng);
 
-        LocalDateTime endDateTime = endDate
-                .map(date -> LocalDateTime.parse(endDate.get() + " 0:00", formatter))
-                .orElse(LocalDateTime.now());
+            DataParamValidator.validateBoundingBox(_sw_lat, _sw_lng, _ne_lat, _ne_lng, 1000);
 
-        double _lat = lat.map(Double::parseDouble).get();
-        double _lng = lng.map(Double::parseDouble).get();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
 
-        double _sw_lat = sw_lat.map(Double::parseDouble).orElse(_lat);
-        double _sw_lng = sw_lng.map(Double::parseDouble).orElse(_lng);
-        double _ne_lat = ne_lat.map(Double::parseDouble).orElse(_lat);
-        double _ne_lng = ne_lng.map(Double::parseDouble).orElse(_lng);
+            LocalDateTime startDateTime = startDate
+                    .map(date -> LocalDateTime.parse(startDate.get() + " 0:00", formatter))
+                    .orElse(LocalDateTime.now().minusMonths(3));
 
-        count++;
-        String logStr = String.format("DataCrime %d: %s %s %s %s: ", count, _sw_lat, _sw_lng, _ne_lat, _ne_lng);
-        log.info(logStr);
+            LocalDateTime endDateTime = endDate
+                    .map(date -> LocalDateTime.parse(endDate.get() + " 0:00", formatter))
+                    .orElse(LocalDateTime.now());
 
-        return dataCrimeService
-            .findAllByBoundsOrderByReportedAtDescGeoJSON(_sw_lat, _sw_lng, _ne_lat, _ne_lng,
-                                                         startDateTime, endDateTime, MAX_LIMIT);
+            count++;
+            String logStr = String.format("DataCrime %d: %s %s %s %s: ", count, _sw_lat, _sw_lng, _ne_lat, _ne_lng);
+            log.info(logStr);
+
+            FeatureCollectionDTO results = dataCrimeService
+                    .findAllByBoundsOrderByReportedAtDescGeoJSON(_sw_lat, _sw_lng, _ne_lat, _ne_lng,
+                            startDateTime, endDateTime, MAX_LIMIT);
+
+            return ResponseEntity.ok().body(results);
+
+        } catch (Exception e) {
+            log.error("[DataCrimeController ERR]: " + e.getMessage());
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
 }
