c85e80869fe0c8085f5694df15b8f06d90e8afdf
vergeman <alan.verga@gmail.com>
2024-11-22 16:27:27 -0600

    DataURLCacheService: add notes on use case

--- api/src/main/java/com/quirkshop/nuisancemaps/service/DataURLCacheService.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/service/DataURLCacheService.java
@@ -14,6 +14,9 @@ public class DataURLCacheService {
     @Autowired
     DataURLCacheRepository dataURLCacheRepository;
 
+    // used to track counts of non-cached requests
+    // eventually want a list of popular locations to warm cache after new data
+    // is input
     @Transactional
     public void increment(DataURLCache dataURLCache) {
         if (dataURLCacheRepository.incrementCountByUrl(dataURLCache.getUrl()) == 0) {
