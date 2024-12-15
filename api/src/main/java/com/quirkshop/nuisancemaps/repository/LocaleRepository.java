4308435152fe0cdacc6880a21f3e070c1ab6cb7e
vergeman <alan.verga@gmail.com>
2024-12-14 18:52:53 -0600

    [api] LocaleRepository:findAllWithSources() query; set associated Source.mapping association to FetchType.LAZY

--- api/src/main/java/com/quirkshop/nuisancemaps/repository/LocaleRepository.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/repository/LocaleRepository.java
@@ -1,11 +1,16 @@
 package com.quirkshop.nuisancemaps.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import com.quirkshop.nuisancemaps.model.Locale;
 
 @Repository
 public interface LocaleRepository extends CrudRepository<Locale, Integer> {
 
+    @Query("SELECT l FROM Locale l LEFT JOIN FETCH l.sources")
+    List<Locale> findAllWithSources();
 }
