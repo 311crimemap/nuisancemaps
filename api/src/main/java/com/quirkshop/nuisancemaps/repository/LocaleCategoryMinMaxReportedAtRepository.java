e1c17e7386a41580862e263e50d5ce11136de516
vergeman <alan.verga@gmail.com>
2024-12-16 11:15:59 -0600

    [api] WorkerScheduleSErvice: add scheduled call (20min) to refresh view, with 5min initial delay LocaleCategoryMinMaxReportedAtRepository:refreshMaterializedView repository method to refresh view

--- api/src/main/java/com/quirkshop/nuisancemaps/repository/LocaleCategoryMinMaxReportedAtRepository.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/repository/LocaleCategoryMinMaxReportedAtRepository.java
@@ -1,5 +1,6 @@
 package com.quirkshop.nuisancemaps.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,8 @@ public interface LocaleCategoryMinMaxReportedAtRepository
 
     @Query(value = "SELECT * FROM locale_category_min_max_reported_at ORDER BY locale_id, category", nativeQuery = true)
     List<LocaleCategoryMinMaxReportedAt> findAll();
+
+    @Modifying
+    @Query(value = "REFRESH MATERIALIZED VIEW locale_category_min_max_reported_at", nativeQuery = true)
+    void refreshMaterializedView();
 }
