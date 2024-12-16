98aee1930d22804ffc8681f3a4ba80dcbafc90fe
vergeman <alan.verga@gmail.com>
2024-12-16 09:36:49 -0600

    Navbar.tsx: add z-index 10 to Navbar to cover page content

--- web/src/Navbar.tsx
+++ web/src/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav data-theme="dracula" className="navbar bg-base-100 fixed">
+    <nav data-theme="dracula" className="navbar bg-base-100 fixed z-10">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl" id="homelink">
           311 Crime Map
