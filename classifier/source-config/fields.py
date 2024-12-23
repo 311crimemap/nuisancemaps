0a3a6df57e92cec8725929cc525b52f8f18541c0
vergeman <alan.verga@gmail.com>
2024-12-22 19:15:42 -0600

    fields.py: helper to view mapped field values from data.csv for quicker verification

---
+++ classifier/source-config/fields.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env/ python3
+
+#
+# takes $DIR/data.csv and prints output form assign fields for quick verification
+#
+
+import argparse
+import os
+import json
+import subprocess
+
+parser = argparse.ArgumentParser(description="generate source config java methods via openAI API")
+parser.add_argument('data', type=str, help="city data subdirectory: 039-buffalo-crime")
+args = parser.parse_args()
+
+DIR=f"{args.data}"
+META_FILE = f"./{DIR}/meta.json"
+SOURCE_CONFIG_FILE = f"./{DIR}/source_config.json"
+
+with open(SOURCE_CONFIG_FILE, 'r') as file:
+    source_config = json.loads(file.read())
+
+mapping = source_config['mapping']
+
+
+fields = [
+    'reportNum',
+    'reportCategory',
+    'description',
+    'address',
+    'location',
+    'latitude',
+    'longitude',
+    'reportedAt',
+    'reportedAt2'
+]
+
+mappedFields = [mapping[field]['field'] for field in fields if mapping[field]['field']]
+csvFields = ",".join(mappedFields)
+command = f"csvcut -c '{csvFields}' {DIR}/data.csv | csvlook -"
+result = subprocess.run(command, shell=True, check=True)
