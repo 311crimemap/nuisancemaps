94428e918894a1f9a615e2965566831f82b00a06
vergeman <alan.verga@gmail.com>
2025-01-06 12:53:11 -0600

    fix(web): use HelmetProvider context, remove Helmet dynamic context in Meta component
    Meta: add path property to generate canonical url
    
    fixes bad sync with meta title and urls

--- web/src/Meta.tsx
+++ web/src/Meta.tsx
@@ -1,18 +1,20 @@
 import { Helmet, HelmetData } from "react-helmet-async";
-
+import { useState, useEffect } from "react";
 interface MetaProps {
   pageName: string;
+  path: string;
 }
 
-export default function Meta({ pageName }: MetaProps) {
-  const helmetData = new HelmetData({});
+export default function Meta({ pageName, path }: MetaProps) {
   const title = pageName ? `311 Crime Map - ${pageName}` : "311 Crime Map";
+  const canonicalPath = path || "";
 
   return (
-        <Helmet helmetData={helmetData}>
-            <title>{title}</title>
-            <meta property="og:title" content={title} />
-            <meta property="twitter:title" content={title} />
-        </Helmet>
-    );
+    <Helmet key={title}>
+      <title>{title}</title>
+      <link rel="canonical" href={`https://311crimemap.com/${canonicalPath}`} />
+      <meta property="og:title" content={title} />
+      <meta property="twitter:title" content={title} />
+    </Helmet>
+  );
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,7 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Navbar from "./Navbar.tsx";
@@ -61,7 +63,9 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+    </React.StrictMode>
 );
