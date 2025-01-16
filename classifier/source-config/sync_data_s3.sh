a9878bd7e5f5e769c11ee7fff05701a956699c81
vergeman <alan.verga@gmail.com>
2025-01-16 13:10:59 -0600

    refactor(classifier): move s3 bucket for sync_config to env config
    minor cleanup in docker-compose.yml

--- classifier/source-config/sync_data_s3.sh
+++ classifier/source-config/sync_data_s3.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
-aws s3 sync --region us-east-2 --profile 311crimemap data s3://311crimemap-data-source-config
+#
+# requires ~/.aws/credentials
+#
+
+source ../../.env.dev
+
+AWS_SYNC_REGION=$AWS_SYNC_REGION
+AWS_SYNC_PROFILE=$AWS_SYNC_PROFILE
+AWS_SYNC_BUCKET=$AWS_SYNC_BUCKET
+
+aws s3 sync --region $AWS_SYNC_REGION --profile $AWS_SYNC_PROFILE data s3://$AWS_SYNC_BUCKET
