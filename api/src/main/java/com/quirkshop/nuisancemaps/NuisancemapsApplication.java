dc1ddfab401e22fe5c8bc709d0d7cf22b0c4b673
vergeman <alan.verga@gmail.com>
2025-01-21 12:29:55 -0600

    cleanup(api): Spring application main classes - remove unused log, imports

--- api/src/main/java/com/quirkshop/nuisancemaps/NuisancemapsApplication.java
+++ api/src/main/java/com/quirkshop/nuisancemaps/NuisancemapsApplication.java
@@ -1,17 +1,12 @@
 package com.quirkshop.nuisancemaps;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import com.quirkshop.nuisancemaps.service.WorkerScheduleService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import com.quirkshop.nuisancemaps.repository.SourceRepository;
-import com.quirkshop.nuisancemaps.service.WorkerScheduleService;
-import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
 
 @SpringBootApplication
 @EnableCaching
@@ -23,17 +18,8 @@ import com.quirkshop.nuisancemaps.repository.DataCrimeRepository;
         })
 public class NuisancemapsApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(NuisancemapsApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(NuisancemapsApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner doesntmatterwhatthisiscalled(SourceRepository srepo, DataCrimeRepository crepo) {
-        return args -> {
-            log.info("[NuisancemapsApplication] CommandLineRunner");
-        };
     }
 
 }
