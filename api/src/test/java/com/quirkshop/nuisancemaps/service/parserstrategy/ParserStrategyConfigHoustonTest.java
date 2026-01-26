bd3858c716971e023d8b6417f7864108465e1f00
Alan Verga <alan.verga@gmail.com>
2026-01-26 15:49:42 -0600

    Topic/data update 1 (#174)
    * fix(dev env): update traefik docker image to traefik:v2.11.31
    
    * fix(doc): README.md fix errant copy-pasted command
    
    * fix(cron): update cron job Houston file to retrieve csv instead of deprecated xls
    
    * fix(data): update Houston crime data to parse changed CSV fields
    
    * update(env): upgrade spring boot to 3.5.10
    
    * fix(ops): tune deployment memory settings

--- api/src/test/java/com/quirkshop/nuisancemaps/service/parserstrategy/ParserStrategyConfigHoustonTest.java
+++ api/src/test/java/com/quirkshop/nuisancemaps/service/parserstrategy/ParserStrategyConfigHoustonTest.java
@@ -31,4 +31,52 @@ public class ParserStrategyConfigHoustonTest {
         assertThat(value).isEqualTo("2021-01-08T00:45:40");
     }
 
+    @Test
+    @Transactional
+    public void REPORTED_AT_CSV_CRIME_HOUSTON_TEST() throws JsonMappingException, JsonProcessingException {
+        assertThat(ParserStrategy.REPORTED_AT_CSV_CRIME_HOUSTON).isNotNull();
+
+        Map<String, String> row = Map.of(
+                "Occurrence Date", "2025-01-01",
+                "Occurrence Hour", "0");
+
+        Map<String, String> row2 = Map.of(
+                "Occurrence Date", "2025-11-15",
+                "Occurrence Hour", "21");
+
+        String value = ParserStrategyConfigHouston.REPORTED_AT_CSV_CRIME_HOUSTON(row, null);
+        assertThat(value).isEqualTo("2025-01-01T00:00:00");
+
+        String value2 = ParserStrategyConfigHouston.REPORTED_AT_CSV_CRIME_HOUSTON(row2, null);
+        assertThat(value2).isEqualTo("2025-11-15T21:00:00");
+    }
+
+    @Test
+    @Transactional
+    public void ADDRESSS_CSV_CRIME_HOUSTON_TEST() throws JsonMappingException, JsonProcessingException {
+        assertThat(ParserStrategy.ADDRESS_CSV_CRIME_HOUSTON).isNotNull();
+
+        Map<String, String> row = Map.of(
+                "Street Number", "4722",
+                "Street Name", "OLD SPANISH",
+                "Street Type", "",
+                "Street Suffix", "",
+                "City", "HOUSTON",
+                "ZIP Code", "706255");
+
+        Map<String, String> row2 = Map.of(
+                "Street Number", "4722",
+                "Street Name", "OLD SPANISH",
+                "Street Type", "TRL",
+                "Street Suffix", "N",
+                "City", "HOUSTON",
+                "ZIP Code", "706255");
+
+        String value = ParserStrategyConfigHouston.ADDRESS_CSV_CRIME_HOUSTON(row, null);
+        assertThat(value).isEqualTo("4722 OLD SPANISH HOUSTON 706255");
+
+        String value2 = ParserStrategyConfigHouston.ADDRESS_CSV_CRIME_HOUSTON(row2, null);
+        assertThat(value2).isEqualTo("4722 N OLD SPANISH TRL HOUSTON 706255");
+
+    }
 }
